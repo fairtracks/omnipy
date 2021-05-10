72fd289d7c2084e9fb7eda4b62d1bae28db11593
Sveinung Gundersen <sveinugu@gmail.com>
2021-05-10 13:22:15 +0200

    Added initial pandas utility functions

---
+++ unifair/unifair/constants.py
@@ -0,0 +1,11 @@
+# DIRECTORY NAMES
+
+REF_DATA_DIR = 'ref_data'
+DATA_DIR = 'data'
+
+# FILE SUFFIXES
+
+CSV_SUFFIX = '.csv'
+HTML_SUFFIX = '.html'
+JSON_HEADERS_SUFFIX = '.headers.json'
+JSON_DATA_SUFFIX = '.json'
