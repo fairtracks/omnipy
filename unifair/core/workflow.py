d6436633a6db542c03ba8b642d2392960631dd07
Sveinung Gundersen <sveinugu@gmail.com>
2021-05-21 17:33:19 +0200

    Initial version of ENCODE workflow (first step partly implemented)

---
+++ unifair/core/workflow.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+
+class WorkflowStep(ABC):
+    @abstractmethod
+    def _get_input_data_cls(self):
+        pass
+
+    @abstractmethod
+    def _get_output_data_cls(self):
+        pass
+
+    def run(self, input_data):
+        self._validate_data_object(input_data, self._get_input_data_cls(), input=True)
+        output_data = self._run(input_data)
+        self._validate_data_object(output_data, self._get_output_data_cls(), input=False)
+        return output_data
+
+    @staticmethod
+    def _validate_data_object(data_obj, data_cls, input):
+        assert isinstance(data_obj, data_cls), \
+            '{} data object is not a subclass of {}, as required.'.format(
+                'Input' if input else 'Output', data_cls
+            )
+        data_obj.validate()
+
+    @abstractmethod
+    def _run(self, input_data):
+        pass
+
+
+class Workflow:
+    def __init__(self):
+        self._steps = []
+
+    def add_step(self, step):
+        assert isinstance(step, WorkflowStep)
+        self._steps.append(step)
+
+    def run(self, input):
+        cur_data = input
+        for step in self._steps:
+            cur_data = step.run(cur_data)
+        return cur_data
