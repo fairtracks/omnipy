de191c44cd8faecaa05442f716805e26c8dc7d15
Sveinung Gundersen <sveinugu@gmail.com>
2022-04-21 15:27:15 +0200

    Changed dataset keys used in tests. Added a few validation tests of JsonDataset

--- tests/tasks/test_transform.py
+++ tests/tasks/test_transform.py
@@ -7,23 +7,23 @@ from unifair.tasks.transform import convert_json_to_pandas
 
 def test_convert_to_pandas():
     json_data = JsonDataset()
-    json_data['obj_1'] = '[{"a": "abc", "b": 12}, {"a": "bcd", "b": 34}]'
-    json_data['obj_2'] = '[{"c": 12, "d": "aaa", "e": "xyz"}, {"c": 23, "d": "bbb", "e": "zyx"}]'
+    json_data['obj_type_1'] = '[{"a": "abc", "b": 12}, {"a": "bcd", "b": 34}]'
+    json_data['obj_type_2'] = '[{"c": 12, "d": "aaa", "e": "xyz"}, {"c": 23, "d": "bbb", "e": "zyx"}]'
 
     pandas_data = convert_json_to_pandas(json_data)
 
     assert isinstance(pandas_data, PandasDataset)
-    assert pandas_data['obj_1'].equals(pd.DataFrame({'a': ['abc', 'bcd'], 'b': [12, 34]}))
-    assert pandas_data['obj_2'].equals(pd.DataFrame({'c': [12, 23], 'd': ['aaa', 'bbb'], 'e': ['xyz', 'zyx']}))
+    assert pandas_data['obj_type_1'].equals(pd.DataFrame({'a': ['abc', 'bcd'], 'b': [12, 34]}))
+    assert pandas_data['obj_type_2'].equals(pd.DataFrame({'c': [12, 23], 'd': ['aaa', 'bbb'], 'e': ['xyz', 'zyx']}))
 
 
 def test_convert_to_pandas_no_records():
     json_data = JsonDataset()
-    json_data['obj'] = '[]'
+    json_data['obj_type'] = '[]'
 
     pandas_data = convert_json_to_pandas(json_data)
 
     assert isinstance(pandas_data, PandasDataset)
-    assert pandas_data['obj'].equals(pd.DataFrame())
+    assert pandas_data['obj_type'].equals(pd.DataFrame())
 
 
