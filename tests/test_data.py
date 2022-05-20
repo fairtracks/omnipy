b3320a6265bfca2204ab144a40c5a056a65ac622
Sveinung Gundersen <sveinugu@gmail.com>
2022-05-20 03:38:21 +0200

    Revert "Fixed poetry environment activation"
    This reverts commit b9cad9cc3752d18847a6cf785c9f1f7cf2865e7f.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,11 @@
 import json
 import os
-from tempfile import TemporaryDirectory
 
 import pandas as pd
-from pandas.testing import assert_frame_equal
 
-from unifair.core.data import JsonDocumentCollection, PandasDataFrameCollection
+from pandas.testing import assert_frame_equal
+from tempfile import TemporaryDirectory
+from unifair.core.data import PandasDataFrameCollection, JsonDocumentCollection
 
 
 def test_pandas_dataframe_add():
@@ -22,20 +22,18 @@ def test_pandas_dataframe_readwrite():
     df_coll_2 = PandasDataFrameCollection()
 
     name = 'data1'
-    df = pd.DataFrame.from_dict({
-        'A': [1, 2, 3],
-        'B': ["{'a': [1, 2, 3]}", "{'b': [2, 4, 6]}", "{'c': [3, 6, 9]}"]
-    })
+    df = pd.DataFrame.from_dict({'A': [1, 2, 3], 'B': ["{'a': [1, 2, 3]}", "{'b': [2, 4, 6]}", "{'c': [3, 6, 9]}"]})
     df_coll_1[name] = df
 
     with TemporaryDirectory() as tmpdirname:
         df_coll_1.write_to_dir(tmpdirname)
 
         with open(os.path.join(tmpdirname, name + '.csv')) as out_file:
-            assert out_file.read() == os.linesep.join([
-                ',A,B', '0,1,"{\'a\': [1, 2, 3]}"', '1,2,"{\'b\': [2, 4, 6]}"',
-                '2,3,"{\'c\': [3, 6, 9]}"', ''
-            ])
+            assert out_file.read() == os.linesep.join([',A,B',
+                                                       '0,1,"{\'a\': [1, 2, 3]}"',
+                                                       '1,2,"{\'b\': [2, 4, 6]}"',
+                                                       '2,3,"{\'c\': [3, 6, 9]}"',
+                                                       ''])
 
         df_coll_2.read_from_dir(tmpdirname)
         assert_frame_equal(df_coll_1[name], df_coll_2[name])
@@ -55,34 +53,44 @@ def test_json_document_readwrite():
     df_coll_2 = JsonDocumentCollection()
 
     name = 'data1'
-    obj = {
-        'A': [1, 2, 3],
-        'B': [{
-            'a': [1, 2, 3]
-        }, {
-            'b': [2, 4, 6]
-        }, {
-            'c': [3, 6, 9]
-        }]
-    }
+    obj = {'A': [1, 2, 3], 'B': [{'a': [1, 2, 3]}, {'b': [2, 4, 6]}, {'c': [3, 6, 9]}]}
     df_coll_1[name] = obj
 
     with TemporaryDirectory() as tmpdirname:
         df_coll_1.write_to_dir(tmpdirname)
 
         with open(os.path.join(tmpdirname, name + '.json')) as out_file:
-            assert out_file.read() == os.linesep.join([
-                '{', '    "A": [', '        1,', '        2,', '        3',
-                '    ],', '    "B": [', '        {', '            "a": [',
-                '                1,', '                2,',
-                '                3', '            ]', '        },',
-                '        {', '            "b": [', '                2,',
-                '                4,', '                6', '            ]',
-                '        },', '        {', '            "c": [',
-                '                3,', '                6,',
-                '                9', '            ]', '        }', '    ]',
-                '}', ''
-            ])
+            assert out_file.read() == os.linesep.join(['{',
+                                                       '    "A": [',
+                                                       '        1,',
+                                                       '        2,',
+                                                       '        3',
+                                                       '    ],',
+                                                       '    "B": [',
+                                                       '        {',
+                                                       '            "a": [',
+                                                       '                1,',
+                                                       '                2,',
+                                                       '                3',
+                                                       '            ]',
+                                                       '        },',
+                                                       '        {',
+                                                       '            "b": [',
+                                                       '                2,',
+                                                       '                4,',
+                                                       '                6',
+                                                       '            ]',
+                                                       '        },',
+                                                       '        {',
+                                                       '            "c": [',
+                                                       '                3,',
+                                                       '                6,',
+                                                       '                9',
+                                                       '            ]',
+                                                       '        }',
+                                                       '    ]',
+                                                       '}',
+                                                       ''])
 
         df_coll_2.read_from_dir(tmpdirname)
         assert df_coll_1[name] == df_coll_2[name]
