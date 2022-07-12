71f7b939d1c11a8b4ea3d00fe5eac69995a6d103
Sveinung Gundersen <sveinugu@gmail.com>
2022-07-12 19:39:55 +0200

    Implemented from_json and added update parameter to to_json in both Dataset and Model. Updated JsonDataset and serializer to use new classes. Updated tests

--- tests/dataset/test_json.py
+++ tests/dataset/test_json.py
@@ -8,58 +8,58 @@ from unifair.dataset.json import JsonDataset
 
 def test_json_dataset_array_of_objects_same_keys():
     json_data = JsonDataset()
-    json_data['obj_type'] = '[{"a": "abc", "b": 12}, {"a": "bcd", "b": 23}]'
+    json_data.from_json({'obj_type': '[{"a": "abc", "b": 12}, {"a": "bcd", "b": 23}]'})
     assert json_data['obj_type'] == [{'a': 'abc', 'b': 12}, {'a': 'bcd', 'b': 23}]
 
 
 def test_json_dataset_array_of_objects_different_keys():
     json_data = JsonDataset()
-    json_data['obj_type'] = '[{"a": "abc", "b": 12}, {"c": "bcd"}]'
+    json_data.from_json({'obj_type': '[{"a": "abc", "b": 12}, {"c": "bcd"}]'})
     assert json_data['obj_type'] == [{'a': 'abc', 'b': 12}, {'c': 'bcd'}]
 
 
 def test_json_dataset_array_of_nested_objects():
     json_data = JsonDataset()
-    json_data['obj_type'] = '[{"a": "abc", "b": {"c": [1, 3]}}]'
+    json_data.from_json({'obj_type': '[{"a": "abc", "b": {"c": [1, 3]}}]'})
     assert json_data['obj_type'] == [{'a': 'abc', 'b': {'c': [1, 3]}}]
 
 
 def test_json_dataset_empty_array():
     json_data = JsonDataset()
-    json_data['obj_type'] = '[]'
+    json_data.from_json({'obj_type': '[]'})
 
 
 def test_json_dataset_empty_array_with_whitespace():
     json_data = JsonDataset()
-    json_data['obj_type'] = ' [] '
+    json_data.from_json({'obj_type': ' [] '})
 
 
 def test_json_dataset_error_no_json():
     json_data = JsonDataset()
-    with pytest.raises(JSONDecodeError):
-        json_data['obj_type'] = ''
+    with pytest.raises(ValidationError):
+        json_data.from_json({'obj_type': ''})
 
 
 def test_json_dataset_error_malformed_json():
     json_data = JsonDataset()
-    with pytest.raises(JSONDecodeError):
-        json_data['obj_type'] = '[{,'
+    with pytest.raises(ValidationError):
+        json_data.from_json({'obj_type': '[{,'})
 
 
 def test_json_dataset_error_not_array():
     json_data = JsonDataset()
     with pytest.raises(ValidationError):
-        json_data['obj_type'] = '1'
+        json_data.from_json({'obj_type': '1'})
 
 
 def test_json_dataset_error_array_elements_not_objects():
     json_data = JsonDataset()
     with pytest.raises(ValidationError):
-        json_data['obj_type'] = '[123, 234]'
+        json_data.from_json({'obj_type': '[123, 234]'})
 
 
 def test_json_dataset_error_empty_object():
     # We might want to reevaluate how to handle empty objects later
     json_data = JsonDataset()
     with pytest.raises(ValidationError):
-        json_data['obj_type'] = '[{"a": "abc", "b": 12}, {}]'
+        json_data.from_json({'obj_type': '[{"a": "abc", "b": 12}, {}]'})
--- tests/dataset/test_serialize_json.py
+++ tests/dataset/test_serialize_json.py
@@ -6,8 +6,7 @@ def test_json_dataset_serializer_to_tar_file():
     json_data = JsonDataset()
     obj_type_1_json = '[{"a": "abc", "b": 12}, {"a": "bcd", "b": 23}]'
     obj_type_2_json = '[{"a": "abc", "b": 12}, {"c": "bcd"}]'
-    json_data['obj_type.1'] = obj_type_1_json
-    json_data['obj_type.2'] = obj_type_2_json
+    json_data.from_json({'obj_type.1': obj_type_1_json, 'obj_type.2': obj_type_2_json})
 
     serializer = JsonDatasetToTarFileSerializer()
     tarfile_bytes = serializer.serialize(json_data)
