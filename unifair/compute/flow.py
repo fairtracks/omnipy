727ea47e3a9599a0867a259cbf1508a4e5f8ebaa
Sveinung Gundersen <sveinugu@gmail.com>
2022-08-29 19:54:47 +0200

    Added non-working integration tests for DAG and function-based flows, as well as for tasks with Dataset and MultiModelDataset inputs/output. Added empty classes to fix collecting of tests in pytest

---
+++ unifair/compute/flow.py
@@ -0,0 +1,22 @@
+class FlowTemplate:
+    ...
+
+
+class DagFlowTemplate(FlowTemplate):
+    pass
+
+
+class FuncFlowTemplate(FlowTemplate):
+    pass
+
+
+class Flow:
+    ...
+
+
+class DagFlow(Flow):
+    pass
+
+
+class FuncFlow(Flow):
+    pass
