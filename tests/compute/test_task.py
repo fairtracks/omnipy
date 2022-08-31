d839906f29d50c2c4c7b6b1f209911fce7a6075a
Sveinung Gundersen <sveinugu@gmail.com>
2022-08-31 14:44:13 +0200

    Forgot test_task.py in last commit

--- tests/compute/test_task.py
+++ tests/compute/test_task.py
@@ -13,64 +13,31 @@ def simple_func():
 
     return simple_func
 
-    # mytask = TaskTemplate(simple_func, fixed_params={'text': 'Number'})
-    # mytask_template = mytask.refine(name='mytask')
-    # mytask = mytask_template.apply()
-    # mytask_template_2 = mytask.revise()
 
-
-def test_simple_task_run(simple_func):
-    mytask = TaskTemplate(simple_func).apply()
-    assert mytask('Number', 12) == 'Number: 12'
-
-    assert mytask.param_signatures == {
+def _assert_default_values(task_obj: Union[TaskTemplate, Task]):
+    assert task_obj.param_signatures == {
         'text': Parameter('text', Parameter.POSITIONAL_OR_KEYWORD, annotation=str),
         'number': Parameter('number', Parameter.POSITIONAL_OR_KEYWORD, annotation=int)
     }
-    assert mytask.return_type == str
+    assert task_obj.return_type == str
 
-    assert mytask.name == 'simple_func'
-    assert mytask.param_map_keys == {}
-    assert mytask.result_key is None
-    assert mytask.fixed_params == {}
-
-
-def _assert_config_properties_immutable(task_obj: Union[TaskTemplate, Task]):
-    with pytest.raises(AttributeError):
-        task_obj.param_signatures = {}  # noqa
-
-    with pytest.raises(TypeError):
-        task_obj.param_signatures['new'] = Parameter(
-            'new', Parameter.POSITIONAL_OR_KEYWORD, annotation=bool)
-
-    with pytest.raises(AttributeError):
-        task_obj.return_type = int
-
-    with pytest.raises(AttributeError):
-        task_obj.name = 'cool_func'
-
-    with pytest.raises(AttributeError):
-        task_obj.param_map_keys = {'text': 'title'}
-
-    with pytest.raises(TypeError):
-        task_obj.param_map_keys['text'] = 'title'
-
-    with pytest.raises(AttributeError):
-        task_obj.result_key = 'cool_func'
-
-    with pytest.raises(AttributeError):
-        task_obj.fixed_params = {'title': 'Integer'}
-
-    with pytest.raises(TypeError):
-        task_obj.fixed_params['title'] = 'Integer'
+    assert task_obj.name == 'simple_func'
+    assert task_obj.param_key_map == {}
+    assert task_obj.result_key is None
+    assert task_obj.fixed_params == {}
 
 
-def test_task_properties_immutable(simple_func):
+def test_simple_task_run_in_default_state(simple_func):
     task_template = TaskTemplate(simple_func)
-    _assert_config_properties_immutable(task_template)
+    assert isinstance(task_template, TaskTemplate)
 
     task = task_template.apply()
-    _assert_config_properties_immutable(task)
+    assert isinstance(task, Task)
+
+    assert task('Number', 12) == 'Number: 12'
+
+    _assert_default_values(task_template)
+    _assert_default_values(task)
 
 
 @pytest.fixture
@@ -82,7 +49,11 @@ def power_func():
 
 
 def test_runtime_task_parameters(power_func):
-    power = TaskTemplate(power_func).apply()
+    power_template = TaskTemplate(power_func)
+    with pytest.raises(TypeError):
+        power_template()
+
+    power = power_template.apply()
 
     assert power(4, 3) == 63
     assert 'exponent' not in power.fixed_params
@@ -109,13 +80,132 @@ def test_error_missing_runtime_task_parameters(power_func):
         power(4, minus_one=False)
 
 
+def test_name(power_func):
+    power_template = TaskTemplate(power_func, name=None)
+    assert power_template.name == 'power_func'
+
+    power = power_template.apply()
+    assert power.name == 'power_func'
+
+    power_template = TaskTemplate(power_func, name='power')
+    assert power_template.name == 'power'
+
+    power = power_template.apply()
+    assert power.name == 'power'
+
+    with pytest.raises(ValueError):
+        TaskTemplate(power_func, name='')
+
+    power_template = TaskTemplate(power_func, name=123)
+    assert power_template.name == '123'
+
+    power = power_template.apply()
+    assert power.name == '123'
+
+
+def test_task_result_key(power_func):
+    power_template = TaskTemplate(power_func, result_key=None)
+    assert power_template.result_key is None
+
+    power = power_template.apply()
+    assert power.result_key is None
+    assert power(4, 2) == 15
+
+    power_template = TaskTemplate(power_func, result_key='power')
+    assert power_template.result_key == 'power'
+
+    power = power_template.apply()
+    assert power.result_key == 'power'
+    assert power(4, 2) == {'power': 15}
+
+    with pytest.raises(ValueError):
+        TaskTemplate(power_func, result_key='')
+
+    power_template = TaskTemplate(power_func, result_key=123)
+    assert power_template.result_key == '123'
+
+    power = power_template.apply()
+    assert power.result_key == '123'
+    assert power(4, 2) == {'123': 15}
+
+
+def test_task_param_key_map(power_func):
+    power_template = TaskTemplate(
+        power_func, param_key_map=dict(
+            number='n',
+            exponent='e',
+            minus_one='m',
+        ))
+
+    assert power_template.param_key_map == {'number': 'n', 'exponent': 'e', 'minus_one': 'm'}
+
+    power = power_template.apply()
+    assert power.param_key_map == dict(number='n', exponent='e', minus_one='m')
+
+    assert power(n=4, e=2) == 15
+
+    with pytest.raises(TypeError):
+        power(number=4, exponent=2)
+
+    power_template = TaskTemplate(
+        power_func, param_key_map=[('number', 'n'), ('exponent', 'e')])  # noqa
+    assert power_template.param_key_map == {'number': 'n', 'exponent': 'e'}
+
+    with pytest.raises(ValueError):
+        TaskTemplate(power_func, param_key_map={'number': 'n', 'exponent': 'n'})
+
+    power_template = TaskTemplate(power_func, param_key_map={'number': 'n'})  # noqa
+    assert power_template.param_key_map == {'number': 'n'}
+
+    power = power_template.apply()
+    assert power.param_key_map == {'number': 'n'}
+
+    assert power(n=3, exponent=2) == 8
+
+    power_template = TaskTemplate(power_func, param_key_map=None)  # noqa
+    assert power_template.param_key_map == {}
+
+    power_template = TaskTemplate(power_func, param_key_map={})  # noqa
+    assert power_template.param_key_map == {}
+
+    power_template = TaskTemplate(power_func, param_key_map=[])  # noqa
+    assert power_template.param_key_map == {}
+
+    power = power_template.apply()
+    assert power.param_key_map == {}
+
+    assert power(number=3, exponent=2) == 8
+
+
 def test_fixed_task_parameters(power_func):
-    square = TaskTemplate(power_func, fixed_params=dict(exponent=2, minus_one=False)).apply()
+    square_template = TaskTemplate(power_func, fixed_params=dict(exponent=2, minus_one=False))
+    square = square_template.apply()
     assert square(4) == 16
     assert square.fixed_params['exponent'] == 2
     assert square.fixed_params['minus_one'] is False
 
     assert square(number=5) == 25
+
+    seven_template = TaskTemplate(power_func, fixed_params=[('number', 2), ('exponent', 3)])  # noqa
+    seven = seven_template.apply()
+
+    assert seven() == 7
+    assert seven_template.fixed_params == {'number': 2, 'exponent': 3}
+    assert seven.fixed_params == {'number': 2, 'exponent': 3}
+
+    power_template = TaskTemplate(power_func, fixed_params=None)  # noqa
+    assert power_template.fixed_params == {}
+
+    power_template = TaskTemplate(power_func, fixed_params={})
+    assert power_template.fixed_params == {}
+
+    power_template = TaskTemplate(power_func, fixed_params=[])  # noqa
+    assert power_template.fixed_params == {}
+
+    power = power_template.apply()
+    assert power.param_key_map == {}
+
+    assert power(number=2, exponent=3) == 7
 
 
 def test_error_missing_or_duplicate_runtime_task_params_with_fixed(power_func):
@@ -131,11 +221,242 @@ def test_error_missing_or_duplicate_runtime_task_params_with_fixed(power_func):
         square(4, minus_one=False)
 
 
-def test_error_parameter_not_in_func_signature(power_func):
+def test_error_fixed_param_not_in_func_signature(power_func):
     with pytest.raises(KeyError):
         TaskTemplate(power_func, fixed_params=dict(engine='hyperdrive'))
 
-    task = TaskTemplate(power_func)
+    task_template = TaskTemplate(power_func)
+
+    assert 'engine' not in task_template.fixed_params
+
+
+def _assert_config_properties_immutable(task_obj: Union[TaskTemplate, Task]):
+    with pytest.raises(AttributeError):
+        task_obj.param_signatures = {}  # noqa
+
+    with pytest.raises(TypeError):
+        task_obj.param_signatures['new'] = Parameter(
+            'new', Parameter.POSITIONAL_OR_KEYWORD, annotation=bool)
+
+    with pytest.raises(AttributeError):
+        task_obj.return_type = int
+
+    with pytest.raises(AttributeError):
+        task_obj.name = 'cool_func'
+
+    with pytest.raises(AttributeError):
+        task_obj.param_key_map = {'text': 'title'}
+
+    with pytest.raises(TypeError):
+        task_obj.param_key_map['text'] = 'title'
+
+    with pytest.raises(AttributeError):
+        task_obj.result_key = 'cool_func'
+
+    with pytest.raises(AttributeError):
+        task_obj.fixed_params = {'title': 'Integer'}
+
+    with pytest.raises(TypeError):
+        task_obj.fixed_params['title'] = 'Integer'
+
+
+def test_task_properties_immutable(simple_func):
+    task_template = TaskTemplate(simple_func)
+    _assert_config_properties_immutable(task_template)
+
+    task = task_template.apply()
+    _assert_config_properties_immutable(task)
+
+
+def test_refine_task_template_with_fixed_params(power_func):
+    power_template = TaskTemplate(power_func)
+    power = power_template.apply()
+
+    assert power(4, 2) == 15
+
+    square_template = power_template.refine(fixed_params=dict(exponent=2, minus_one=False))
+    assert square_template.fixed_params == dict(exponent=2, minus_one=False)
+
+    square = square_template.apply()
+
+    assert square.fixed_params['minus_one'] is False
+    assert square(3) == 9
+
+    cube_template = square_template.refine(fixed_params=[('exponent', 3)])  # noqa
+    assert cube_template.fixed_params == dict(exponent=3, minus_one=False)
+
+    cube = cube_template.apply()
+
+    assert cube.fixed_params['minus_one'] is False
+    assert cube(3) == 27
+
+    hypercube_template = cube_template.refine(fixed_params=dict(exponent=4), update=False)
+    assert hypercube_template.fixed_params == dict(exponent=4)
+
+    hypercube = hypercube_template.apply()
+
+    assert hypercube.fixed_params.get('minus_one') is None
+    assert hypercube(3) == 80
+
+    reset_power_template = hypercube_template.refine(fixed_params={})
+    assert reset_power_template.fixed_params == dict(exponent=4)
+
+    reset_power_template = hypercube_template.refine(fixed_params={}, update=False)
+    assert reset_power_template.fixed_params == {}
+
+    reset_power_template = hypercube_template.refine(fixed_params=[], update=False)  # noqa
+    assert reset_power_template.fixed_params == {}
+
+    reset_power_template = hypercube_template.refine(fixed_params=None, update=False)  # noqa
+    assert reset_power_template.fixed_params == {}
+
+    reset_power = reset_power_template.apply()
+
+    assert reset_power.fixed_params.get('minus_one') is None
+    assert reset_power(4, 2) == 15
+
+
+def test_refine_task_template_with_config_change(power_func):
+    power_template = TaskTemplate(power_func)
+    power = power_template.apply()
+
+    assert power(4, 2) == 15
+
+    my_power_template = power_template.refine(
+        name='my_power',
+        param_key_map=dict(number='num', exponent='exp'),
+        result_key='my_power_result',
+    )
+    my_power = my_power_template.apply()
+
+    assert my_power(num=3, exp=3) == {'my_power_result': 26}
+    assert my_power.name == 'my_power'
+    assert my_power.param_key_map == dict(number='num', exponent='exp')
+    assert my_power.result_key == 'my_power_result'
+
+    my_power_template = my_power_template.refine(
+        param_key_map=[('exponent', 'expo'), ('minus_one', 'min')],)  # noqa
+    my_power = my_power_template.apply()
+
+    assert my_power(num=3, expo=3, min=False) == {'my_power_result': 27}
+    assert my_power.name == 'my_power'
+    assert my_power.param_key_map == dict(number='num', exponent='expo', minus_one='min')
+    assert my_power.result_key == 'my_power_result'
+
+    my_power_template = power_template.refine(
+        param_key_map=dict(exponent='expo', minus_one='min'), update=False)
+    my_power = my_power_template.apply()
+
+    assert my_power(number=3, expo=3, min=False) == 27
+    assert my_power.name == 'power_func'
+    assert my_power.param_key_map == dict(exponent='expo', minus_one='min')
+    assert my_power.result_key is None
+
+    my_new_power_template = my_power_template.refine(param_key_map={})
+    assert my_new_power_template.param_key_map == dict(exponent='expo', minus_one='min')
+
+    my_new_power_template = my_power_template.refine(param_key_map={}, update=False)
+    assert my_new_power_template.param_key_map == {}
+
+    my_new_power_template = my_power_template.refine(param_key_map=[], update=False)  # noqa
+    assert my_new_power_template.param_key_map == {}
+
+    my_new_power_template = my_power_template.refine(param_key_map=None, update=False)  # noqa
+    assert my_new_power_template.param_key_map == {}
+
+    my_new_power = my_new_power_template.apply()
+
+    assert my_new_power(number=3, exponent=3, minus_one=False) == 27
+    assert my_new_power.param_key_map == {}
+
+
+def test_revise_refine_task_template_with_fixed_params_and_config_change(power_func):
+    square = TaskTemplate(power_func).refine(
+        name='square',
+        param_key_map=dict(number='num', exponent='exp'),
+        fixed_params=dict(exponent=2, minus_one=False),
+    ).apply()
+
+    assert square(num=3) == 9
+
+    assert square.name == 'square'
+    assert square.param_key_map == dict(number='num', exponent='exp')
+    assert square.result_key is None
+    assert square.fixed_params == {'exponent': 2, 'minus_one': False}
+
+    square_revised_template = square.revise()
+    assert isinstance(square_revised_template, TaskTemplate)
+
+    assert square_revised_template.name == 'square'
+    assert square_revised_template.param_key_map == dict(number='num', exponent='exp')
+    assert square_revised_template.result_key is None
+    assert square_revised_template.fixed_params == {'exponent': 2, 'minus_one': False}
+
+    square_refined = square_revised_template.refine(
+        name=square_revised_template.name,
+        fixed_params=square_revised_template.fixed_params,
+        update=False).apply()
+
+    assert square_refined.name == 'square'
+    assert square_refined.param_key_map == {}
+    assert square_refined.result_key is None
+    assert square_refined.fixed_params == {'exponent': 2, 'minus_one': False}
+
+    power_reset = square_refined.revise().refine(update=False).apply()
+
+    assert power_reset.name == 'power_func'
+    assert power_reset.param_key_map == {}
+    assert power_reset.result_key is None
+    assert power_reset.fixed_params == {}
+
+
+# TODO: test that different TaskTemplates and Tasks do not edit same dicts
+def test_no_shared_dicts(power_func):
+    square_template = TaskTemplate(
+        power_func,
+        name='square',
+        param_key_map=dict(number='num'),
+        fixed_params=dict(exponent=2),
+    )
+
+    square = square_template.apply()
+    square_refined_template = square_template.refine(
+        param_key_map=dict(exponent='exp'),
+        fixed_params=dict(minus_one=False),
+    )
+    square_revised_template = square.revise()
+    square_revised_refined_template = square_revised_template.refine(
+        param_key_map=dict(exponent='exp'),
+        fixed_params=dict(minus_one=False),
+    )
+
+    assert len(square_refined_template.param_key_map) == 2
+    assert len(square_refined_template.fixed_params) == 2
+
+    assert len(square_revised_refined_template.param_key_map) == 2
+    assert len(square_revised_refined_template.fixed_params) == 2
+
+    assert len(square_revised_template.param_key_map) == 1
+    assert len(square_revised_template.fixed_params) == 1
+
+    assert len(square_template.param_key_map) == 1
+    assert len(square_template.fixed_params) == 1
+
+    assert len(square.param_key_map) == 1
+    assert len(square.fixed_params) == 1
+
+
+def test_error_refine_task_unmatched_params(power_func):
+    power_template = TaskTemplate(power_func)
 
     with pytest.raises(KeyError):
-        _mytask = task.fixed_params['engine']
+        _power_template = TaskTemplate(power_func, param_key_map={'engine': 'motor'})
+
+    with pytest.raises(KeyError):
+        _power_template = TaskTemplate(power_func, fixed_params={'engine': 'volkswagen'})
+
+    with pytest.raises(KeyError):
+        _cube = power_template.refine(param_key_map={'engine': 'horsepower'})
+
+    with pytest.raises(KeyError):
+        _cube = power_template.refine(fixed_params={'engine': 'toyota'})
