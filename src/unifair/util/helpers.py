bce9e212243b1e9bd5ff6ab1d198a195dbec37fc
Sveinung Gundersen <sveinugu@gmail.com>
2022-09-04 11:43:46 +0200

    Small fix to type annotation

--- src/unifair/util/helpers.py
+++ src/unifair/util/helpers.py
@@ -1,9 +1,8 @@
-from types import MappingProxyType
-from typing import Any, Dict, Union
+from typing import Any, Dict, Mapping, Union
 
 
-def create_merged_dict(dict_1: Union[Dict[Any, Any], MappingProxyType[Any, Any]],
-                       dict_2: Union[Dict[Any, Any], MappingProxyType[Any, Any]]) -> Dict[Any, Any]:
+def create_merged_dict(dict_1: Union[Mapping[Any, Any]],
+                       dict_2: Union[Mapping[Any, Any]]) -> Dict[Any, Any]:
     merged_dict = dict(dict_1)
     merged_dict.update(dict_2)
     return merged_dict
