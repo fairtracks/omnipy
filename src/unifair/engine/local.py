e771d36abde03ab6519ccd676d94ecab4d1acc35
Sveinung Gundersen <sveinugu@gmail.com>
2022-09-14 19:20:26 +0200

    Updated LocalRunner stub to follow new Engine class

--- src/unifair/engine/local.py
+++ src/unifair/engine/local.py
@@ -4,18 +4,5 @@ from unifair.engine.base import Engine
 
 
 class LocalRunner(Engine):
-    @staticmethod
-    def task_decorator() -> Callable:
-        pass
-
-    @staticmethod
-    def result_persisting_task_decorator(result_dir: str) -> Callable:
-        pass
-
-    @staticmethod
-    def flow_decorator() -> Callable:
-        pass
-
-    @staticmethod
-    def executable_task_decorator() -> Callable:
-        pass
+    def _init_engine(self) -> None:
+        ...
