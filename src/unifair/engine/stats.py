a64e4f7743b64391dc1728b54c5f7456d7faef5c
Sveinung Gundersen <sveinugu@gmail.com>
2022-09-15 13:59:22 +0200

    Added support for run statistic logging + small fixes

--- src/unifair/engine/stats.py
+++ src/unifair/engine/stats.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
 from datetime import datetime
-from enum import IntEnum
-from typing import DefaultDict, Dict, List, Optional, Tuple
+from enum import Enum, IntEnum
+import logging
+from typing import DefaultDict, Dict, List, Optional, Tuple, Union
 
 from unifair.engine.protocols import TaskProtocol
+from unifair.util.helpers import get_datetime_format
 
 
 class State(IntEnum):
@@ -12,12 +14,52 @@ class State(IntEnum):
     FINISHED = 3
 
 
+class StateLogMessages(Enum):
+    INITIALIZED: str = 'Initialized task "{}"'
+    RUNNING: str = 'Started running task "{}"...'
+    FINISHED: str = 'Task "{}" finished!'
+
+
+UNIFAIR_LOG_FORMAT_STR: str = '%(levelname)s (%(name)s) - %(message)s'
+
+
 class RunStats:
     def __init__(self) -> None:
+        self._datetime_format: Optional[str] = None
+        self._logger: Optional[logging.Logger] = None
+
         self._tasks: Dict[str, TaskProtocol] = {}
         self._task_states: Dict[str, State] = {}
         self._state_tasks: DefaultDict[State, List[str]] = defaultdict(list)
         self._task_state_datetime: Dict[Tuple[str, State], datetime] = {}
+
+    def get_task_state(self, task: TaskProtocol) -> State:
+        return self._task_states[task.name]
+
+    def get_task_state_datetime(self, task: TaskProtocol, state: State) -> datetime:
+        return self._task_state_datetime[(task.name, state)]
+
+    def all_tasks(self, state: Optional[State] = None) -> Tuple[TaskProtocol, ...]:
+        if state is not None:
+            task_names = self._state_tasks[state]
+            return tuple(self._tasks[name] for name in task_names)
+        else:
+            return tuple(self._tasks.values())
+
+    def set_logger(self,
+                   logger: logging.Logger,
+                   set_unifair_formatter_on_handlers=True,
+                   locale: Union[str, Tuple[str, str]] = '') -> None:
+
+        self._logger = logger
+        self._datetime_format = get_datetime_format(locale)
+
+        if set_unifair_formatter_on_handlers:
+            formatter = logging.Formatter(UNIFAIR_LOG_FORMAT_STR)
+
+            for handler in self._logger.handlers:
+                if set_unifair_formatter_on_handlers:
+                    handler.setFormatter(formatter)
 
     def set_task_state(self, task: TaskProtocol, state: State) -> None:
         cur_datetime = datetime.now()
@@ -28,8 +70,9 @@ class RunStats:
             self._register_new_task(task, state)
 
         self._update_task_stats(task, state, cur_datetime)
+        self._log_state_change(task, state)
 
-    def _update_task_registration(self, task: TaskProtocol, state: State):
+    def _update_task_registration(self, task: TaskProtocol, state: State) -> None:
         if id(self._tasks[task.name]) != id(task):
             self._raise_task_error(
                 task,
@@ -45,7 +88,7 @@ class RunStats:
                 f'to state {state.name} is not allowed',
             )
 
-    def _register_new_task(self, task, state):
+    def _register_new_task(self, task, state) -> None:
         if state != State.INITIALIZED:
             self._raise_task_error(
                 task,
@@ -53,23 +96,16 @@ class RunStats:
             )
         self._tasks[task.name] = task
 
-    def _update_task_stats(self, task, state, cur_datetime):
+    def _update_task_stats(self, task, state, cur_datetime) -> None:
         self._task_states[task.name] = state
         self._state_tasks[state].append(task.name)
         self._task_state_datetime[(task.name, state)] = cur_datetime
 
-    def _raise_task_error(self, task: TaskProtocol, msg: str):
+    def _log_state_change(self, task: TaskProtocol, state: State) -> None:
+        if self._logger is not None:
+            datetime_str = self.get_task_state_datetime(task, state).strftime(self._datetime_format)
+            log_msg = StateLogMessages[state.name].value.format(task.name)
+            self._logger.info(f'{datetime_str}: {log_msg}')
+
+    def _raise_task_error(self, task: TaskProtocol, msg: str) -> None:
         raise ValueError(f'Error in task "{task.name}": {msg}')
-
-    def get_task_state(self, task: TaskProtocol) -> State:
-        return self._task_states[task.name]
-
-    def get_task_state_datetime(self, task: TaskProtocol, state: State) -> datetime:
-        return self._task_state_datetime[(task.name, state)]
-
-    def all_tasks(self, state: Optional[State] = None) -> Tuple[TaskProtocol, ...]:
-        if state is not None:
-            task_names = self._state_tasks[state]
-            return tuple(self._tasks[name] for name in task_names)
-        else:
-            return tuple(self._tasks.values())
--- tests/engine/test_run_stats.py
+++ tests/engine/test_run_stats.py
@@ -1,11 +1,16 @@
+from copy import copy
 from datetime import datetime, timedelta
+from io import StringIO
+import logging
 from time import sleep
 from typing import Annotated
 
 import pytest
 
+from engine.helpers.functions import read_log_line_from_stream, read_log_lines_from_stream
 from unifair.engine.protocols import TaskProtocol
 from unifair.engine.stats import RunStats, State
+from unifair.util.helpers import get_datetime_format
 
 
 def test_task_state_transitions(task_a: Annotated[TaskProtocol, pytest.fixture],
@@ -145,6 +150,7 @@ def test_fail_task_key_error(task_a: Annotated[TaskProtocol, pytest.fixture],
 
 def test_fail_task_same_name_different_task(task_a: Annotated[TaskProtocol, pytest.fixture],
                                             task_b: Annotated[TaskProtocol, pytest.fixture]):
+    task_b = copy(task_b)
     task_b.name = task_a.name
 
     run_stats = RunStats()
@@ -155,3 +161,104 @@ def test_fail_task_same_name_different_task(task_a: Annotated[TaskProtocol, pyte
 
     with pytest.raises(ValueError):
         run_stats.set_task_state(task_b, State.RUNNING)
+
+
+def test_state_change_logging(
+    str_stream: Annotated[StringIO, pytest.fixture],
+    simple_logger: Annotated[logging.Logger, pytest.fixture],
+    task_a: Annotated[TaskProtocol, pytest.fixture],
+    task_b: Annotated[TaskProtocol, pytest.fixture],
+):
+    run_stats = RunStats()
+
+    simple_logger.addHandler(logging.StreamHandler(str_stream))
+    run_stats.set_logger(simple_logger)
+
+    events = [
+        (task_a, State.INITIALIZED),
+        (task_a, State.RUNNING),
+        (task_b, State.INITIALIZED),
+        (task_a, State.FINISHED),
+        (task_b, State.RUNNING),
+        (task_b, State.FINISHED),
+    ]
+
+    datetime_list = []
+    for task, state in events:
+        run_stats.set_task_state(task, state)
+        datetime_list.append(run_stats.get_task_state_datetime(task, state))
+
+    log_lines = read_log_lines_from_stream(str_stream)
+
+    assert len(log_lines) == 6
+
+    assert log_lines[0] == f'INFO (test) - ' \
+                           f'{datetime_list[0].strftime(get_datetime_format())}: ' \
+                           f'Initialized task "a"'
+    assert log_lines[1] == f'INFO (test) - ' \
+                           f'{datetime_list[1].strftime(get_datetime_format())}: ' \
+                           f'Started running task "a"...'
+    assert log_lines[2] == f'INFO (test) - ' \
+                           f'{datetime_list[2].strftime(get_datetime_format())}: ' \
+                           f'Initialized task "b"'
+    assert log_lines[3] == f'INFO (test) - ' \
+                           f'{datetime_list[3].strftime(get_datetime_format())}: ' \
+                           f'Task "a" finished!'
+    assert log_lines[4] == f'INFO (test) - ' \
+                           f'{datetime_list[4].strftime(get_datetime_format())}: ' \
+                           f'Started running task "b"...'
+    assert log_lines[5] == f'INFO (test) - ' \
+                           f'{datetime_list[5].strftime(get_datetime_format())}: ' \
+                           f'Task "b" finished!'
+
+
+def test_state_change_logging_handler_formatting_variants(
+    str_stream: Annotated[StringIO, pytest.fixture],
+    simple_logger: Annotated[logging.Logger, pytest.fixture],
+    task_a: Annotated[TaskProtocol, pytest.fixture],
+):
+    run_stats = RunStats()
+
+    # Handler added after set_logger
+    run_stats.set_logger(simple_logger, set_unifair_formatter_on_handlers=True)
+    simple_logger.addHandler(logging.StreamHandler(str_stream))
+    run_stats.set_task_state(task_a, State.INITIALIZED)
+
+    log_line = read_log_line_from_stream(str_stream)
+    assert 'INFO (test)' not in log_line
+
+    # Handler added before set_logger, set_unifair_formatter_on_handlers=False
+    run_stats.set_logger(simple_logger, set_unifair_formatter_on_handlers=False)
+    run_stats.set_task_state(task_a, State.RUNNING)
+
+    log_line = read_log_line_from_stream(str_stream)
+    assert 'INFO (test)' not in log_line
+
+    # Handler added before set_logger, set_unifair_formatter_on_handlers=True
+    run_stats.set_logger(simple_logger, set_unifair_formatter_on_handlers=True)
+    run_stats.set_task_state(task_a, State.FINISHED)
+
+    log_line = read_log_line_from_stream(str_stream)
+
+    assert 'INFO (test)' in log_line
+
+
+def test_state_change_logging_date_localization(
+    str_stream: Annotated[StringIO, pytest.fixture],
+    simple_logger: Annotated[logging.Logger, pytest.fixture],
+    task_a: Annotated[TaskProtocol, pytest.fixture],
+):
+    run_stats = RunStats()
+
+    simple_logger.addHandler(logging.StreamHandler(str_stream))
+
+    locale = ('no_NO', 'UTF-8')
+    run_stats.set_logger(simple_logger, locale=locale)
+
+    run_stats.set_task_state(task_a, State.INITIALIZED)
+    init_datetime = run_stats.get_task_state_datetime(task_a, State.INITIALIZED)
+
+    log_line = read_log_line_from_stream(str_stream)
+
+    assert init_datetime.strftime(get_datetime_format(locale)) in log_line
+    assert 'INFO (test)' in log_line
