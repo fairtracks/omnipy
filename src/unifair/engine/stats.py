af33aaf3c2dd40f14d243b486adf227002924caa
Sveinung Gundersen <sveinugu@gmail.com>
2022-09-15 06:27:01 +0200

    Added datetime statistics

--- src/unifair/engine/stats.py
+++ src/unifair/engine/stats.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import datetime
 from enum import IntEnum
 from typing import DefaultDict, Dict, List, Optional, Tuple
 
@@ -16,8 +17,11 @@ class RunStats:
         self._tasks: Dict[str, TaskProtocol] = {}
         self._task_states: Dict[str, State] = {}
         self._state_tasks: DefaultDict[State, List[str]] = defaultdict(list)
+        self._task_state_datetime: Dict[Tuple[str, State], datetime] = {}
 
     def set_task_state(self, task: TaskProtocol, state: State) -> None:
+        cur_datetime = datetime.now()
+
         if task.name in self._tasks:
             if id(self._tasks[task.name]) != id(task):
                 self._raise_task_error(
@@ -40,14 +44,19 @@ class RunStats:
                     f'Initial state of must be "INITIALIZED", not "{state.name}"',
                 )
             self._tasks[task.name] = task
+
         self._task_states[task.name] = state
         self._state_tasks[state].append(task.name)
+        self._task_state_datetime[(task.name, state)] = cur_datetime
 
     def _raise_task_error(self, task: TaskProtocol, msg: str):
         raise ValueError(f'Error in task "{task.name}": {msg}')
 
     def get_task_state(self, task: TaskProtocol) -> State:
         return self._task_states[task.name]
+
+    def get_task_state_datetime(self, task: TaskProtocol, state: State) -> datetime:
+        return self._task_state_datetime[(task.name, state)]
 
     def all_tasks(self, state: Optional[State] = None) -> Tuple[TaskProtocol, ...]:
         if state is not None:
