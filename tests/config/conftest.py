64ea375d2c02088e81d6cac9a62febd01082a082
Sveinung Gundersen <sveinugu@gmail.com>
2022-09-18 05:42:25 +0200

    Refactored ConfigPublisher tests

---
+++ tests/config/conftest.py
@@ -0,0 +1,39 @@
+from typing import Annotated, Callable, List
+
+import pytest
+
+from config.helpers.mock import MockConfigPublisher, MockSubscriberCls
+
+
+@pytest.fixture(scope='function')
+def subscriber_obj() -> MockSubscriberCls:
+    return MockSubscriberCls()
+
+
+@pytest.fixture(scope='function')
+def text_list() -> List[str]:
+    return []
+
+
+@pytest.fixture(scope='function')
+def list_appender_subscriber_func(
+        text_list: Annotated[List[str], pytest.fixture]) -> Callable[[str], None]:
+    def list_appender_subscriber_func(text: str) -> None:
+        text_list.append(text)
+
+    return list_appender_subscriber_func
+
+
+@pytest.fixture(scope='function')
+def mock_config_publisher_with_subscribers(
+    subscriber_obj: Annotated[MockSubscriberCls, pytest.fixture],
+    list_appender_subscriber_func: Annotated[Callable[[str], None], pytest.fixture],
+) -> MockConfigPublisher:
+
+    config = MockConfigPublisher()
+
+    config.subscribe('foo', subscriber_obj.set_foo)
+    config.subscribe('text', list_appender_subscriber_func)
+    config.subscribe('text', subscriber_obj.set_text)
+
+    return config
