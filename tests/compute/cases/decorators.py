6903ea31a21b5ce4bea5d858d72115d4162887ae
Sveinung Gundersen <sveinugu@gmail.com>
2022-12-04 17:46:08 +0100

    Added test for running tasks and flows as decorators
    ...including iterative flows

---
+++ tests/compute/cases/decorators.py
@@ -0,0 +1,101 @@
+import pytest_cases as pc
+
+from unifair.compute.flow import DagFlowTemplate, FuncFlowTemplate
+from unifair.compute.task import TaskTemplate
+
+
+@pc.case(
+    id='task-plus_one(number)',
+    tags=['sync', 'function', 'task', 'plain'],
+)
+def case_task_plus_one_template() -> TaskTemplate:
+    @TaskTemplate
+    def plus_one(number: int) -> int:
+        return number + 1
+
+    return plus_one  # noqa  # Pycharm static type checker bug
+
+
+@pc.case(
+    id='task-plus_other(number, other=1)',
+    tags=['sync', 'function', 'task', 'with_kw_params'],
+)
+def case_task_plus_other_as_plus_one_template() -> TaskTemplate:
+    @TaskTemplate(
+        name='plus_one',
+        fixed_params=dict(other=1),
+    )
+    def plus_other(number: int, other: int) -> int:
+        return number + other
+
+    return plus_other  # noqa  # Pycharm static type checker bug
+
+
+@pc.case(
+    id='dag_flow-plus_five(number)',
+    tags=['sync', 'function', 'dag_flow', 'plain'],
+)
+@pc.parametrize_with_cases('plus_one_template', cases='.', has_tag='task')
+def case_dag_flow_number_plus_five_template(plus_one_template) -> DagFlowTemplate:
+
+    iterative_number_plus_one_template = plus_one_template.refine(result_key='number')
+
+    @DagFlowTemplate(*((iterative_number_plus_one_template,) * 5))
+    def plus_five(number: int) -> int:  # noqa
+        ...
+
+    return plus_five  # noqa  # Pycharm static type checker bug
+
+
+@pc.case(
+    id='dag_flow-plus_five(x)',
+    tags=['sync', 'function', 'dag_flow', 'with_kw_params'],
+)
+@pc.parametrize_with_cases('plus_one_template', cases='.', has_tag='task')
+def case_dag_flow_x_plus_five_template(plus_one_template) -> FuncFlowTemplate:
+    # TODO: Expand on this example with param_key_map and result_key, given these
+    #       are reimplemented as mixins
+
+    iterative_x_plus_one_template = plus_one_template.refine(
+        param_key_map=dict(number='x'),
+        result_key='x',
+    )
+
+    @DagFlowTemplate(*((iterative_x_plus_one_template,) * 5))
+    def plus_five(x: int) -> int:  # noqa
+        ...
+
+    return plus_five  # noqa  # Pycharm static type checker bug
+
+
+@pc.case(
+    id='func_flow-plus_y(number, y)',
+    tags=['sync', 'function', 'func_flow', 'plain'],
+)
+@pc.parametrize_with_cases('plus_one_template', cases='.', has_tag='task')
+def case_func_flow_plus_y_template(plus_one_template) -> FuncFlowTemplate:
+    @FuncFlowTemplate
+    def plus_y(number: int, y: int) -> int:
+        for _ in range(y):
+            number = plus_one_template(number=number)
+        return number
+
+    return plus_y  # noqa  # Pycharm static type checker bug
+
+
+@pc.case(
+    id='func_flow-plus_func(x, y)',
+    tags=['sync', 'function', 'func_flow', 'with_kw_params'],
+)
+@pc.parametrize_with_cases('plus_one_template', cases='.', has_tag='task')
+def case_func_flow_plus_function_as_plus_y_template(plus_one_template) -> FuncFlowTemplate:
+    # TODO: Expand on this example with param_key_map and result_key, given these
+    #       are reimplemented as mixins
+
+    @FuncFlowTemplate(name='plus_y')
+    def plus_function(x: int, y: int) -> int:
+        for _ in range(y):
+            x = plus_one_template(number=x)
+        return x
+
+    return plus_function  # noqa  # Pycharm static type checker bug
---
+++ tests/compute/test_decorators.py
@@ -0,0 +1,96 @@
+import inspect
+from typing import Annotated, Callable
+
+import pytest
+import pytest_cases as pc
+
+from compute.helpers.mocks import MockLocalRunner
+from unifair.compute.flow import DagFlow, DagFlowTemplate, FuncFlow, FuncFlowTemplate
+from unifair.compute.task import Task, TaskTemplate
+
+
+@pc.parametrize_with_cases(
+    'plus_one_template',
+    cases='.cases.decorators',
+    has_tag='task',
+)
+def test_task_template_as_decorator(mock_local_runner: Annotated[MockLocalRunner, pytest.fixture],
+                                    plus_one_template: TaskTemplate) -> None:
+    assert isinstance(plus_one_template, TaskTemplate)  # noqa  # Pycharm static type checker bug
+    assert plus_one_template.name == 'plus_one'  # noqa  # Pycharm static type checker bug
+
+    plus_one = plus_one_template.apply()  # noqa  # Pycharm static type checker bug
+    assert isinstance(plus_one, Task)
+
+    assert plus_one(number=3) == 4
+
+
+@pc.parametrize_with_cases(
+    'plus_five_template',
+    cases='.cases.decorators',
+    has_tag='dag_flow',
+)
+def test_dag_flow_template_as_decorator(mock_local_runner: Annotated[MockLocalRunner,
+                                                                     pytest.fixture],
+                                        plus_five_template: DagFlowTemplate) -> None:
+
+    assert (plus_five_template, DagFlowTemplate)
+    assert plus_five_template.name == 'plus_five'
+
+    plus_five = plus_five_template.apply()
+    assert isinstance(plus_five, DagFlow)
+
+    result = plus_five(3)
+    assert isinstance(result, dict)
+    assert len(result) == 1
+    key, val = result.popitem()
+    assert key in ('number', 'x')
+    assert val == 8
+
+
+@pc.parametrize_with_cases(
+    'plus_y_template',
+    cases='.cases.decorators',
+    has_tag='func_flow',
+)
+def test_func_flow_template_as_decorator(mock_local_runner: Annotated[MockLocalRunner,
+                                                                      pytest.fixture],
+                                         plus_y_template: FuncFlowTemplate) -> None:
+
+    assert (plus_y_template, FuncFlowTemplate)
+    assert plus_y_template.name == 'plus_y'
+
+    plus_y = plus_y_template.apply()
+    assert isinstance(plus_y, FuncFlow)
+
+    assert plus_y(3, y=5) == 8
+
+
+def test_fail_task_template_decorator_with_func_argument() -> None:
+    with pytest.raises(TypeError):
+
+        def myfunc(a: Callable) -> Callable:
+            return a
+
+        @TaskTemplate(myfunc)
+        def plus_one(number: int) -> int:
+            return number + 1
+
+
+@pc.parametrize_with_cases(
+    'plus_one_template',
+    cases='.cases.decorators',
+    prefix='case_task_',
+    has_tag='plain',
+)
+def test_fail_job_tmpl_decorator_with_func_argument(mock_local_runner: Annotated[MockLocalRunner,
+                                                                                 pytest.fixture],
+                                                    plus_one_template) -> None:
+    with pytest.raises(TypeError):
+
+        def myfunc(a: Callable) -> Callable:
+            return a
+
+        @FuncFlowTemplate(myfunc)
+        def plus_one(number: int) -> int:
+            return plus_one_template(number)
