595f51bc14b222df2f4c8de4081091f5dd6cc5ca
Sveinung Gundersen <sveinugu@gmail.com>
2022-12-05 17:35:44 +0100

    Refactored engine tests to simplify testing of flows

--- tests/engine/test_task_runner.py
+++ tests/engine/test_task_runner.py
@@ -1,44 +1,39 @@
 import pytest
 import pytest_cases as pc
 
-from .helpers.functions import extract_engine, run_task_test
+from .helpers.classes import JobCase
+from .helpers.functions import extract_engine, run_job_test
 
 
 @pc.parametrize(
-    'task_case',
-    [pc.fixture_ref('power_mock_task_mock_taskrun_subcls_config_no_verbose_reg')],
-    ids=[''],
-)
-def test_task_runner_mock_task_mock_engine_config_no_verbose_reg_sync(task_case) -> None:
-    task, run_and_assert_results = task_case
+    'job_case', [pc.fixture_ref('power_mock_job_mock_runner_subcls_no_verbose_no_reg')], ids=[''])
+def test_job_runners_mock_job_no_verbose_no_reg_sync(job_case: JobCase) -> None:
+    job = job_case.job
 
     for i in range(2):
-        run_and_assert_results(task)
+        job_case.run_and_assert_results_func(job)
 
-    engine = extract_engine(task)
+    engine = extract_engine(job)
 
-    assert task.name == 'power'
+    assert job.name == 'power'
     assert len(engine.finished_backend_tasks) == 2
     for backend_task in engine.finished_backend_tasks:
         assert backend_task.backend_verbose is False
 
 
 @pc.parametrize(
-    'task_case',
-    [
-        pc.fixture_ref('singlethread_mock_task_mock_taskrun_subcls_mock_reg'),
-        pc.fixture_ref('multithread_mock_task_mock_taskrun_subcls_mock_reg'),
-        pc.fixture_ref('multiprocess_mock_task_mock_taskrun_subcls_mock_reg')
-    ],
-    ids=['single', 'multithread', 'multiprocess'],
-)
+    'job_case',
+    [pc.fixture_ref('all_func_types_mock_job_mock_runner_subcls_assert_runstate_mock_reg')],
+    ids=[''])
 @pytest.mark.asyncio
-async def test_task_runner_mock_task_mock_engine_mock_reg(task_case) -> None:
-    for i in range(2):
-        await run_task_test(task_case)
+async def test_job_runners_mock_job_mock_runner_subcls_assert_runstate_mock_reg(
+        job_case: JobCase) -> None:
+    job = job_case.job
 
-    task, _run_and_assert_results = task_case
-    engine = extract_engine(task)
+    for i in range(2):
+        await run_job_test(job_case)
+
+    engine = extract_engine(job)
 
     assert len(engine.finished_backend_tasks) == 2
     for backend_task in engine.finished_backend_tasks:
