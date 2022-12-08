055254efcb6c7a10c56cee1fc40d0a548b3d0b80
Sveinung Gundersen <sveinugu@gmail.com>
2022-12-08 18:46:14 +0100

    Moved ENCODE and GDC import functions from old code to FAIRtracks module

---
+++ src/unifair/modules/fairtracks/functions.py
@@ -0,0 +1,42 @@
+import requests
+
+
+# ['projects', 'cases', 'files', 'annotations'], starting_point='0', size='25'
+def gdc_api(cls, object_type='projects', starting_point=None, size=None):
+    api_url = cls.GDC_BASE_URL + object_type + '/' + '?' + \
+              '&'.join(
+                  (['from=' + starting_point] if starting_point else [])
+                  + (['size=' + size] if size else [])
+                  + (['expand=' + 'project'] if object_type == 'cases' else [])
+              )
+    print(api_url)
+    response = requests.get(api_url)
+    if response.status_code != 200:
+        print('No result found')
+        return None
+
+    results = response.json()
+
+    if len(results['warnings']) > 0:
+        print('The following warnings have been encountered:')
+        print(results['warnings'])
+        return None
+
+    hits = (results['data']['hits'])
+    return hits
+
+
+# ['experiments', 'biosample']
+def encode_api(cls, endpoint='experiments', id=None, limit=None, format='json', frame='object'):
+    api_url = cls.ENCODE_BASE_URL + endpoint + '/' + (id if id else '@@listing') + '?' + '&'.join(
+        (['limit=' + limit] if limit else []) + (['format=' + format] if format else [])
+        + (['frame=' + frame] if frame else []))
+    print(api_url)
+    response = requests.get(api_url, headers=cls.HEADERS)
+    if response.status_code == 200:
+        results = response.json()
+        if results['notification'] == 'Success':
+            graph = results['@graph']
+            return graph
+    else:
+        print('No result found')
