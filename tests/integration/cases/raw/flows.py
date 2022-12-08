70000640961bd15401e2437c723cb3a9abd79b7b
Sveinung Gundersen <sveinugu@gmail.com>
2022-12-08 15:50:00 +0100

    Refactored and updated implementation tests (still failing)

---
+++ tests/integration/cases/raw/flows.py
@@ -0,0 +1,58 @@
+from typing import Dict
+
+from unifair.compute.flow import DagFlowTemplate, FuncFlowTemplate
+from unifair.data.dataset import Dataset, MultiModelDataset
+
+from ...helpers.models import GeneralTable, RecordSchema
+from .tasks import (apply_models_to_dataset,
+                    extract_record_model,
+                    merge_key_value_into_str,
+                    square_root,
+                    uppercase)
+
+
+@DagFlowTemplate(
+    uppercase.refine(result_key='upper'),
+    square_root,
+    merge_key_value_into_str.refine(
+        param_key_map={
+            'key': 'upper',
+            'val': 'pos_root',
+        }, result_key='pos_square_root'))
+def pos_square_root_dag_flow(
+        number: int,  # noqa
+        text: str,  # noqa
+) -> str:
+    ...
+
+
+@FuncFlowTemplate(result_key='pos_square_root')
+def pos_square_root_func_flow(
+    number: int,
+    text: str,
+) -> Dict[str, int]:
+    upper = uppercase(text)
+    _neg_root, pos_root = square_root(number)
+    return merge_key_value_into_str(upper, pos_root)
+
+
+@DagFlowTemplate(
+    extract_record_model.refine(
+        param_key_map={'table': 'data'},
+        result_key='models',  # iterate_over_dataset=True,
+    ),
+    apply_models_to_dataset,
+)
+def specialize_record_models_dag_flow(
+        tables: Dataset[GeneralTable]) -> MultiModelDataset[GeneralTable]:  # noqa
+    ...
+
+
+@FuncFlowTemplate
+def specialize_record_models_func_flow(
+        tables: Dataset[GeneralTable]) -> MultiModelDataset[GeneralTable]:
+
+    record_models = Dataset[RecordSchema]([
+        (table_name, extract_record_model(table)) for table_name, table in tables
+    ])
+    return apply_models_to_dataset(tables, record_models)
---
+++ tests/integration/cases/raw/tasks.py
@@ -0,0 +1,47 @@
+from typing import Any, Dict
+
+from unifair.compute.task import TaskTemplate
+from unifair.data.dataset import Dataset, MultiModelDataset
+
+from ...helpers.models import GeneralTable, RecordSchema
+
+# from .validators import first_dataset_keys_in_all_datasets
+
+
+@TaskTemplate
+def uppercase(text: str) -> str:
+    return text.upper()
+
+
+@TaskTemplate
+def square_root(number: int) -> Dict[str, float]:
+    return {'neg_root': -number**1 / 2, 'pos_root': number**1 / 2}
+
+
+@TaskTemplate
+def merge_key_value_into_str(key: Any, val: Any) -> str:
+    return '{}: {}'.format(key, val)
+
+
+# @TaskTemplate(serializer=PythonSerializer)
+@TaskTemplate()
+def extract_record_model(table: GeneralTable) -> RecordSchema:
+    record_model = {}
+    for record in table:
+        for field_key, field_val in record.items():
+            if field_key not in record_model:
+                record_model[field_key] = type(field_val)
+            else:
+                assert record_model[field_key] == type(field_val)
+
+    return record_model
+
+
+# @TaskTemplate(serializer=CsvSerializer, extra_validators=(first_dataset_keys_in_all_datasets,))
+@TaskTemplate
+def apply_models_to_dataset(dataset: Dataset[GeneralTable],
+                            models: Dataset[RecordSchema]) -> MultiModelDataset[GeneralTable]:
+    multi_model_dataset = dataset.as_multi_model_dataset()
+    for obj_type in multi_model_dataset.keys():
+        multi_model_dataset.set_model(obj_type, models[obj_type])
+    return multi_model_dataset
