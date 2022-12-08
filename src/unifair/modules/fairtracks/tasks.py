d0f244a32f01f515b4051d206a1660368f915f67
Sveinung Gundersen <sveinugu@gmail.com>
2022-12-08 18:46:14 +0100

    Moved ENCODE and GDC import functions from old code to FAIRtracks module

--- src/unifair/modules/fairtracks/tasks.py
+++ src/unifair/modules/fairtracks/tasks.py
@@ -3,9 +3,9 @@ from typing import Iterable
 from pydantic import constr, PositiveInt
 
 from unifair.compute.task import TaskTemplate
-from unifair.old.steps.imports.encode import ImportEncodeMetadataFromApi
 
 from ..json.models import JsonDataset
+from .functions import encode_api
 
 
 @TaskTemplate
@@ -13,7 +13,7 @@ def import_dataset_from_encode(endpoints: Iterable[constr(min_length=1)],
                                max_data_item_count: PositiveInt) -> JsonDataset:
     dataset = JsonDataset()
     for endpoint in endpoints:
-        dataset[endpoint] = ImportEncodeMetadataFromApi.encode_api(
+        dataset[endpoint] = encode_api(
             endpoint,
             limit=str(max_data_item_count),
             format='json',
