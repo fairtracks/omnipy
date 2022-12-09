fd82d9d496b2f847a7b0eae58f1e524de9243658
Sveinung Gundersen <sveinugu@gmail.com>
2022-12-09 02:01:27 +0100

    Reorganized integration tests into novel and reused

--- tests/integration/compute/test_task_with_all_engines.py
+++
@@ -1,4 +0,0 @@
-from ...compute.test_task import test_task_run, test_task_run_parameter_variants
-
-test_task_run  # noqa
-test_task_run_parameter_variants  # noqa
--- tests/integration/engine/test_all_engines_with_real_jobs_and_registry.py
+++
@@ -1,4 +0,0 @@
-from ...engine.test_all_engines import test_mock_tasks_all_engines_mock_registry_job_case  # noqa
-from ...engine.test_all_engines import test_mock_tasks_all_engines_mock_registry
-
-test_mock_tasks_all_engines_mock_registry  # noqa
