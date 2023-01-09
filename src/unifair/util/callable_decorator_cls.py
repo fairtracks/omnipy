1efcb3418f924058f1960d5aec11278758a24433
Sveinung Gundersen <sveinugu@gmail.com>
2023-01-09 17:44:00 +0100

    Callable decorator now wraps outside of WithMixins __init__()s

--- src/unifair/util/callable_decorator_cls.py
+++ src/unifair/util/callable_decorator_cls.py
@@ -66,32 +66,47 @@ def callable_decorator_cls(cls: Type[T]) -> CallableClass[Type[T]]:
             return True
         return False
 
-    _wrapped_init: CallableParamAfterSelf = cast(CallableParamAfterSelf, cls.__init__)
+    _wrapped_new: Callable = cls.__new__
 
-    # Wrapper method that replaces the __init__ method of the decorated class
-    def _init_wrapper(self, *args: object, **kwargs: object) -> None:
-        args_list = list(args)
-
-        def _init(callable_arg: Callable) -> None:
-            _wrapped_init(self, callable_arg, *args_list, **kwargs)
-            update_wrapper(self, callable_arg, updated=[])
-
-        if len(args_list) == 1 and _real_callable(args_list[0]):
-            # Decorate the callable directly
-            _callable_arg: Callable = cast(Callable, args_list[0])
-            args_list.pop(0)
-            _init(_callable_arg)
+    def _new_wrapper(cls, *args: object, **kwargs: object) -> T:
+        if _wrapped_new is object.__new__:
+            obj = _wrapped_new(cls)
         else:
-            # Add an instance-level _obj_call method, which are again callable by the
-            # class-level __call__ method. When this method is called, the provided _callable_arg
-            # is decorated.
-            def _init_as_obj_call_method(self, _callable_arg: Callable) -> Type[T]:  # noqa
+            obj = _wrapped_new(cls, *args, **kwargs)
+            # setattr(cls, '__new__', _wrapped_new)
+
+        _wrapped_init: CallableParamAfterSelf = cast(CallableParamAfterSelf, obj.__class__.__init__)
+
+        # Wrapper method that replaces the __init__ method of the decorated class
+        def _init_wrapper(self, *args: object, **kwargs: object) -> None:
+            args_list = list(args)
+
+            def _init(callable_arg: Callable) -> None:
+                _wrapped_init(self, callable_arg, *args_list, **kwargs)
+                update_wrapper(self, callable_arg, updated=[])
+
+            if len(args_list) == 1 and _real_callable(args_list[0]):
+                # Decorate the callable directly
+                _callable_arg: Callable = cast(Callable, args_list[0])
+                args_list.pop(0)
                 _init(_callable_arg)
-                del self._obj_call
-                return self
+            else:
+                # Add an instance-level _obj_call method, which are again callable by the
+                # class-level __call__ method. When this method is called, the provided _callable_arg
+                # is decorated.
+                def _init_as_obj_call_method(self, _callable_arg: Callable) -> Type[T]:  # noqa
+                    _init(_callable_arg)
+                    del self._obj_call
+                    return self
 
-            self._obj_call = _init_as_obj_call_method.__get__(self)
+                self._obj_call = _init_as_obj_call_method.__get__(self)
 
-    setattr(cls, '__init__', _init_wrapper)
+            setattr(self.__class__, '__init__', _wrapped_init)
+
+        setattr(obj.__class__, '__init__', _init_wrapper)
+
+        return obj
+
+    setattr(cls, '__new__', _new_wrapper)
 
     return cast(CallableClass[Type[T]], cls)
