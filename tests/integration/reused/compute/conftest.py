7e7123335572af248ca1cae7d07696e3b3790259
Sveinung Gundersen <sveinugu@gmail.com>
2023-01-18 16:12:17 +0100

    Fixed lacking fixture for test_flow_with_all_engines

--- tests/integration/reused/compute/conftest.py
+++ tests/integration/reused/compute/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from omnipy.compute.task import TaskTemplate
 
+from ....compute.conftest import all_flow_classes  # noqa
+
 
 @pytest.fixture
 def mock_local_runner(runtime_all_engines):
