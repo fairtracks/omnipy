93244e8bd3bbb9274cd5ddfef3df1e1881cdba2c
Sveinung Gundersen <sveinugu@gmail.com>
2023-02-02 13:17:03 +0100

    Added prefect logging config. Skipped test for now

--- src/omnipy/modules/prefect/__init__.py
+++ src/omnipy/modules/prefect/__init__.py
@@ -1,3 +1,17 @@
+import inspect
+import os
+from pathlib import Path
+
+
+def set_prefect_config_path():
+    prefect_module_dir = os.path.dirname(inspect.getabsfile(inspect.currentframe()))
+
+    os.environ['PREFECT_LOGGING_SETTINGS_PATH'] = \
+        f"{Path(prefect_module_dir).joinpath('settings', 'logging.yml')}"
+
+
+# set_prefect_config_path()
+
 from prefect import flow, Flow, State, task, Task
 from prefect.tasks import task_input_hash
 from prefect.utilities.names import generate_slug
