3afd09d3bbe40960bccec0904b6a1e0fb54f3f9d
Sveinung Gundersen <sveinugu@gmail.com>
2023-02-03 10:43:41 +0100

    Updated dependencies. Removed xfail on test that now runs (with Prefect 2.7.3)

--- tests/engine/cases/tasks.py
+++ tests/engine/cases/tasks.py
@@ -70,14 +70,6 @@ def case_sync_power_kwargs() -> JobCase[[int, int], int]:
 )
 def case_sync_range() -> JobCase[[int], Generator]:
     def run_and_assert_results(job: IsJob) -> None:
-        from omnipy.modules.prefect.engine.prefect import PrefectEngine
-        if check_engine_cls(job, PrefectEngine):
-            pytest.xfail('Synchronous generators stopped working with prefect v2.6.0 (before that,'
-                         'they were running eagerly, returning lists of all yielded values).'
-                         'Seems to be partly a pydantic bug:'
-                         'https://github.com/PrefectHQ/prefect/issues/7692'
-                         'https://github.com/PrefectHQ/prefect/pull/7714')
-
         generator = job(5)
         assert_job_state(job, [RunState.RUNNING, RunState.FINISHED])
 
