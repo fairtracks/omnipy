98766c3d5616c2c2a1454edaaac0707ce98af169
Sveinung Gundersen <sveinugu@gmail.com>
2023-02-13 13:31:50 +0100

    Make sure local Prefect Orion instance is used for tests

--- src/omnipy/modules/prefect/__init__.py
+++ src/omnipy/modules/prefect/__init__.py
@@ -1,6 +1,7 @@
 import inspect
 import os
 from pathlib import Path
+import sys
 
 
 def set_prefect_config_path():
@@ -10,7 +11,14 @@ def set_prefect_config_path():
         f"{Path(prefect_module_dir).joinpath('settings', 'logging.yml')}"
 
 
+def use_local_api_for_tests():
+    if 'pytest' in sys.modules:
+        os.environ['PREFECT_API_KEY'] = ''
+        os.environ['PREFECT_API_URL'] = ''
+
+
 set_prefect_config_path()
+use_local_api_for_tests()
 
 from prefect import flow, Flow, State, task, Task
 from prefect.tasks import task_input_hash
--- tests/modules/prefect/test_prefect.py
+++ tests/modules/prefect/test_prefect.py
@@ -1,10 +1,19 @@
-# @pytest.mark.skipif(os.getenv('OMNIPY_FORCE_SKIPPED_TEST') != '1', reason='To be implemented later')
 import logging
+import os
 
 
 def test_no_root_logger():
     import omnipy.modules.prefect
 
+    assert os.environ['PREFECT_LOGGING_SETTINGS_PATH'].endswith('logging.yml')
+
     assert logging.root.level == logging.WARN
     print(logging.root.handlers)
     assert len(logging.root.handlers) == 0
+
+
+def test_only_local_orion():
+    import omnipy.modules.prefect
+
+    assert os.environ['PREFECT_API_KEY'] == ''
+    assert os.environ['PREFECT_API_URL'] == ''
