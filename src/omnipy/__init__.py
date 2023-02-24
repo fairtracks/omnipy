5f09bb1d6bfba721013a76d05aa07417c00048c5
Sveinung Gundersen <sveinugu@gmail.com>
2023-02-24 12:03:19 +0100

    Refactor omnipy.runtime

--- src/omnipy/__init__.py
+++ src/omnipy/__init__.py
@@ -2,11 +2,17 @@ __version__ = '0.9.0'
 
 import os
 import sys
+from typing import Optional
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-runtime = None
 
-if 'pytest' not in sys.modules:
-    from omnipy.hub.runtime import Runtime
-    runtime = Runtime()
+def _get_runtime() -> Optional['Runtime']:
+    if 'pytest' not in sys.modules:
+        from omnipy.hub.runtime import Runtime
+        return Runtime()
+    else:
+        return None
+
+
+runtime: Optional['Runtime'] = _get_runtime()
