3ca4ee0e53d44e7bbbf8244dfa403dfe426c143c
Sveinung Gundersen <sveinugu@gmail.com>
2023-03-15 11:29:21 +0100

    Temporarily readded TODOs

--- src/omnipy/data/dataset.py
+++ src/omnipy/data/dataset.py
@@ -75,6 +75,8 @@ class Dataset(GenericModel, Generic[ModelT], UserDict):
     data: Dict[str, ModelT] = Field(default={})
 
     def __class_getitem__(cls, model: ModelT) -> ModelT:
+        # TODO: change model type to params: Union[Type[Any], Tuple[Type[Any], ...]]
+        #       as in GenericModel.
 
         # For now, only singular model types are allowed. These lines are needed for
         # interoperability with pydantic GenericModel, which internally stores the model
@@ -91,6 +93,13 @@ class Dataset(GenericModel, Generic[ModelT], UserDict):
     def __init__(self,
                  value: Union[Dict[str, Any], Iterator[Tuple[str, Any]]] = Undefined,
                  **input_data: Any) -> None:
+        # TODO: Error message when forgetting parenthesis when creating Dataset should be improved.
+        #       Unclear where this can be done, if anywhere? E.g.:
+        #           a = Dataset[Model[int]]
+        #           a['adsfas'] = 2
+        #           Traceback (most recent call last):
+        #             ...
+        #           TypeError: 'ModelMetaclass' object does not support item assignment
 
         if value != Undefined:
             input_data[DATA_KEY] = value
