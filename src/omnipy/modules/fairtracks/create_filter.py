408b9875be19debfbcbc426bbfa051865f789aa7
Sveinung Gundersen <sveinugu@gmail.com>
2023-03-24 16:15:03 +0100

    Fixed styling of old GDC script

--- src/omnipy/modules/fairtracks/create_filter.py
+++ src/omnipy/modules/fairtracks/create_filter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import json
-from numpy import arange
 import sys
+
+from numpy import arange
 import requests
 
 #########################################################
@@ -25,139 +26,140 @@ annotations_endpt = 'https://api.gdc.cancer.gov/annotations'
 ###########################################################
 # step1: get total number of TCGA projects (program.name)
 ###########################################################
-params = {'facets':'program.name',
-          'from':0, 'size':0}
-response = requests.get(projects_endpt, params = params)
-buckets = response.json()["data"]["aggregations"]["program.name"]["buckets"]
+params = {
+    'facets': 'program.name',
+    'from': 0,
+    'size': 0,
+}
+response = requests.get(projects_endpt, params=params)
+buckets = response.json()['data']['aggregations']['program.name']['buckets']
 
 for j in arange(len(buckets)):
-    if buckets[j]["key"]=="TCGA":
-        size_max = buckets[j]["doc_count"] 
+    if buckets[j]['key'] == 'TCGA':
+        size_max = buckets[j]['doc_count']
         break
-    
+
 if download_all_projects:
-    size=size_max
+    size = size_max
 else:
-    size=min(size_max, number_of_projects)
+    size = min(size_max, number_of_projects)
 
 ##############################################################
 # step 2: filtered query to get the UIDs of each TCGA project
 ##############################################################
-fields = ["summary.case_count","summary.file_count"]
-fields = ",".join(fields)
+fields = ['summary.case_count', 'summary.file_count']
+fields = ','.join(fields)
 
 filters = {
-    "op": "in",
-    "content":{
-        "field": "program.name",
-        "value": ["TCGA"]
-        }
-    }
+    'op': 'in',
+    'content': {
+        'field': 'program.name',
+        'value': ['TCGA'],
+    },
+}
 
 params = {
-    "filters": json.dumps(filters),
-    "fields": fields,
-    "size": size
-    }
+    'filters': json.dumps(filters),
+    'fields': fields,
+    'size': size,
+}
 
-response = requests.get(projects_endpt, params = params)
+response = requests.get(projects_endpt, params=params)
 
-if len(response.json()['data']['hits'])!=size:
-    print("size mismatch")
-    sys.exit() 
+if len(response.json()['data']['hits']) != size:
+    print('size mismatch')
+    sys.exit()
 
 projects_list = (response.json()['data']['hits'])
 
-
 ##########################################################################
-#Step3: filtered query on "cases" (filter on project_id) to get cases ID for each TCGA project
+#Step3: filtered query on 'cases' (filter on project_id) to get cases ID for each TCGA project
 #      the IDs for all the files are also available form this endpoint
 ########################################################################
 
-fields = ["files.file_id","summary.file_count","annotation"]
-fields = ",".join(fields)
+fields = ['files.file_id', 'summary.file_count', 'annotation']
+fields = ','.join(fields)
 
 for proj in projects_list:
     if download_all_cases:
-        size = proj["summary"]["case_count"]
+        size = proj['summary']['case_count']
     else:
-        size = min(proj["summary"]["case_count"], number_of_cases)
+        size = min(proj['summary']['case_count'], number_of_cases)
     filters = {
-        "op": "in",
-        "content":{
-            "field": "project.project_id",
-            "value": proj["id"]
-            }
-        }
-    params = {
-    "filters": json.dumps(filters),
-    "fields": fields,
-    "size": size
+        'op': 'in',
+        'content': {
+            'field': 'project.project_id',
+            'value': proj['id'],
+        },
     }
-    response = requests.get(cases_endpt, params = params)
-    cases=(response.json()["data"]["hits"])
-    proj["cases"]=cases
-
+    params = {
+        'filters': json.dumps(filters),
+        'fields': fields,
+        'size': size,
+    }
+    response = requests.get(cases_endpt, params=params)
+    cases = (response.json()['data']['hits'])
+    proj['cases'] = cases
 
 ##########################################################################
-#step4: filtered query on "annotations" (filter on case_id) to get cases ID for each TCGA project
+#step4: filtered query on 'annotations' (filter on case_id) to get cases ID for each TCGA project
 #      the IDs for all the files are also available form this endpoint
 ########################################################################
 
-fields = ["annotation_id"]
+fields = ['annotation_id']
 
 for proj in projects_list:
-    for case in proj["cases"]:
+    for case in proj['cases']:
         filters = {
-            "op": "in",
-            "content":{
-                "field": "case_id",
-                "value": case["id"]
-                }
-            }
+            'op': 'in',
+            'content': {
+                'field': 'case_id',
+                'value': case['id'],
+            },
+        }
         params = {
-            "fields": fields,
-            "filters": json.dumps(filters),
-            }
-        response = requests.get(annotations_endpt, params = params)
-        annotations=response.json()["data"]["hits"]
-        case["annotations"]=annotations
+            'fields': fields,
+            'filters': json.dumps(filters),
+        }
+        response = requests.get(annotations_endpt, params=params)
+        annotations = response.json()['data']['hits']
+        case['annotations'] = annotations
 
 ############################################################################
 # step 5: create list of IDs for projects, cases, files and annotations
 #         Use these lists to define filters and write them to file
 ###########################################################################
 
-proj_id_list=[]
-case_id_list=[]
-file_id_list=[]
-ant_id_list=[]
+proj_id_list = []
+case_id_list = []
+file_id_list = []
+ant_id_list = []
 
 for proj in projects_list:
-    proj_id_list.append(proj["id"])
-    for case in proj["cases"]:
-        case_id_list.append(case["id"])
-        for file in case["files"]:
-            file_id_list.append(file["file_id"])
-        for ant in case["annotations"]:
-            ant_id_list.append(ant["annotation_id"])
+    proj_id_list.append(proj['id'])
+    for case in proj['cases']:
+        case_id_list.append(case['id'])
+        for file in case['files']:
+            file_id_list.append(file['file_id'])
+        for ant in case['annotations']:
+            ant_id_list.append(ant['annotation_id'])
 
-file_id_list=file_id_list[0:number_of_files]      
-        
+file_id_list = file_id_list[0:number_of_files]
+
 id_dict = {
-    "projects": proj_id_list, 
-    "cases": case_id_list,
-    "files": file_id_list,
-    "annotations": ant_id_list,
-}        
+    'projects': proj_id_list,
+    'cases': case_id_list,
+    'files': file_id_list,
+    'annotations': ant_id_list,
+}
 
-for entry in ["cases","projects","files","annotations"]:
+for entry in ['cases', 'projects', 'files', 'annotations']:
     filters = {
-        "op": "in",
-        "content":{
-        "field": entry[0:-1]+"_id",
-        "value": id_dict[entry]
-        }
-                }
-    with open(entry+'_filter.json', 'w') as outfile:
+        'op': 'in',
+        'content': {
+            'field': entry[0:-1] + '_id',
+            'value': id_dict[entry],
+        },
+    }
+    with open(entry + '_filter.json', 'w') as outfile:
         json.dump(filters, outfile, indent=4)
