915677c6ad42af675287ae9d9a01250d1f46c21d
bianchini88 <fredebi@uio.no>
2023-03-29 16:17:07 +0200

    refactored with omnipy jobs

--- src/omnipy/modules/fairtracks/create_filter.py
+++ src/omnipy/modules/fairtracks/create_filter.py
@@ -2,11 +2,18 @@
 
 import json
 import sys
+from copy import copy
 from typing import cast, Dict, List, Optional, Union
 
 from pydantic import BaseModel
 import requests
 
+from omnipy.compute.flow import LinearFlowTemplate, DagFlowTemplate
+from omnipy.compute.task import TaskTemplate
+from omnipy.modules.json.datasets import JsonListOfDictsOfScalarsDataset, JsonDictOfScalarsDataset, \
+    JsonDictOfDictsOfScalarsDataset, JsonDataset
+from omnipy.modules.json.models import JsonListOfDictsOfScalarsModel, JsonDictOfScalarsModel, \
+    JsonDictOfDictsOfScalarsModel
 from omnipy.modules.json.types import Json, JsonDict
 
 #########################################################
@@ -51,7 +58,8 @@ def call_endpoint(endpoint: str, filters: Optional[Filter] = None, **kwargs: obj
     return requests.get(endpoint, params=kwargs).json()  # type: ignore
 
 
-def get_project_count_for_program(program: str) -> Optional[int]:
+@TaskTemplate()
+def get_project_counts() -> JsonListOfDictsOfScalarsDataset:
     response = call_endpoint(
         projects_endpt,
         facets='program.name',
@@ -60,93 +68,114 @@ def get_project_count_for_program(program: str) -> Optional[int]:
     )
     buckets = response['data']['aggregations']['program.name']['buckets']
 
-    for bucket in buckets:
-        if bucket['key'] == program:
-            project_count = bucket['doc_count']
-            return project_count
-        raise ValueError(f'Program with name "{program}" could not be found')
+    dataset = JsonListOfDictsOfScalarsDataset()
+    dataset['buckets'] = buckets
+    return dataset
 
 
-size_max = cast(int, get_project_count_for_program('TCGA'))
+@TaskTemplate(iterate_over_data_files=True)
+def create_dict_from_list_of_dicts(list_of_dicts: JsonListOfDictsOfScalarsModel, field_whose_values_will_be_new_keys:str) -> JsonDictOfDictsOfScalarsModel:
+    output_dict = {}
+    for item in list_of_dicts:
+        item_copy = copy(item.contents)
+        new_key = item_copy[field_whose_values_will_be_new_keys]
+        del item_copy[field_whose_values_will_be_new_keys]
+        output_dict[new_key] = item_copy
+    return output_dict
 
-if download_all_projects:
-    size = size_max
-else:
-    size = min(size_max, number_of_projects)
 
-##############################################################
-# step 2: filtered query to get the UIDs of each TCGA project
-##############################################################
-fields = ','.join(['summary.case_count', 'summary.file_count', 'program.name'])
-response = call_endpoint(projects_endpt, filters=create_filter('program.name', ['TCGA']), fields=fields, size=size)
-projects = response['data']['hits']
-if len(projects) != size:
-    print('size mismatch')
-    sys.exit()
 
-##########################################################################
-# Step3: filtered query on 'cases' (filter on project_id) to get cases ID for each TCGA project
-#      the IDs for all the files are also available form this endpoint
-########################################################################
 
-for project in projects:
-    if download_all_cases:
-        size = project['summary']['case_count']
+# TODO: Figure out why JsonDictOfDictsOfScalarsModel was serialized as CSV
+# TODO: Somehow fix so that we do not need to call Model.contents within a task
+
+
+@TaskTemplate()
+def rest_of_the_steps(project_count_per_program: JsonDictOfDictsOfScalarsDataset) -> JsonDataset:
+    ##############################################################
+    # step 2: filtered query to get the UIDs of each TCGA project
+    ##############################################################
+    program_info_dict = project_count_per_program['buckets']['TCGA'].contents
+    size_max = program_info_dict['doc_count']
+
+    if download_all_projects:
+        size = size_max
     else:
-        size = min(project['summary']['case_count'], number_of_cases)
-    response = call_endpoint(
-        cases_endpt, filter=create_filter('project.project_id', [project['id']]),
-        fields=','.join(['files.file_id', 'summary.file_count', 'annotation']), size=size)
-    project['cases'] = response['data']['hits']
+        size = min(size_max, number_of_projects)
 
-##############################################################################
-# step4: filtered query on 'annotations' (filter on case_id) to get cases
-#        ID for each TCGA project the IDs for all the files are also available
-#        form this endpoint
-##############################################################################
+    fields = ','.join(['summary.case_count', 'summary.file_count'])
+    response = call_endpoint(projects_endpt, filters=create_filter('program.name', ['TCGA']), fields=fields, size=size)
+    projects = response['data']['hits']
+    if len(projects) != size:
+        print('size mismatch')
+        sys.exit()
 
-for project in projects:
-    for case in project['cases']:
+    ##########################################################################
+    # Step3: filtered query on 'cases' (filter on project_id) to get cases ID for each TCGA project
+    #        the IDs for all the files are also available form this endpoint
+    ########################################################################
+
+    for project in projects:
+        if download_all_cases:
+            size = project['summary']['case_count']
+        else:
+            size = min(project['summary']['case_count'], number_of_cases)
         response = call_endpoint(
-            annotations_endpt, filter=create_filter('case_id', [case['id']]),
-            fields=['annotation_id'], size=size)
-        case['annotations'] = response['data']['hits']
+            cases_endpt, filter=create_filter('project.project_id', [project['id']]),
+            fields=','.join(['files.file_id', 'summary.file_count', 'annotation']), size=size)
+        project['cases'] = response['data']['hits']
 
-############################################################################
-# step 5: create list of IDs for projects, cases, files and annotations
-#         Use these lists to define filters and write them to file
-###########################################################################
+    ##############################################################################
+    # step4: filtered query on 'annotations' (filter on case_id) to get cases
+    #        ID for each TCGA project the IDs for all the files are also available
+    #        form this endpoint
+    ##############################################################################
 
-proj_id_list = []
-case_id_list = []
-file_id_list = []
-annotation_id_list = []
+    for project in projects:
+        for case in project['cases']:
+            response = call_endpoint(
+                annotations_endpt, filter=create_filter('case_id', [case['id']]),
+                fields=['annotation_id'], size=size)
+            case['annotations'] = response['data']['hits']
 
-for project in projects:
-    proj_id_list.append(project['id'])
-    for case in project['cases']:
-        case_id_list.append(case['id'])
-        for i, file in enumerate(case['files']):
-            if i >= number_of_files:
-                continue
-            file_id_list.append(file['file_id'])
-        for annotation in case['annotations']:
-            annotation_id_list.append(annotation['annotation_id'])
+    ############################################################################
+    # step 5: create list of IDs for projects, cases, files and annotations
+    #         Use these lists to define filters and write them to file
+    ###########################################################################
 
-id_dict = {
-    'projects': proj_id_list,
-    'cases': case_id_list,
-    'files': file_id_list,
-    'annotations': annotation_id_list,
-}
+    proj_id_list = []
+    case_id_list = []
+    file_id_list = []
+    annotation_id_list = []
 
-for entry in ['cases', 'projects', 'files', 'annotations']:
-    filters = {
-        'op': 'in',
-        'content': {
-            'field': entry[0:-1] + '_id',
-            'value': id_dict[entry],
-        },
+    for project in projects:
+        proj_id_list.append(project['id'])
+        for case in project['cases']:
+            case_id_list.append(case['id'])
+            for i, file in enumerate(case['files']):
+                if i >= number_of_files:
+                    continue
+                file_id_list.append(file['file_id'])
+            for annotation in case['annotations']:
+                annotation_id_list.append(annotation['annotation_id'])
+
+    id_dict = {
+        'projects': proj_id_list,
+        'cases': case_id_list,
+        'files': file_id_list,
+        'annotations': annotation_id_list,
     }
-    with open(entry + '_filter.json', 'w') as outfile:
-        json.dump(filters, outfile, indent=4)
+
+    dataset = JsonDataset()
+    for entry in ['cases', 'projects', 'files', 'annotations']:
+        dataset[entry] = create_filter(entry[0:-1] + '_id', id_dict[entry])
+    return dataset
+
+@LinearFlowTemplate(get_project_counts,
+                    create_dict_from_list_of_dicts.refine(name='create_dict_from_list_of_dicts_using_key_field',
+                                                          fixed_params=dict(field_whose_values_will_be_new_keys='key')),
+                    rest_of_the_steps)
+def get_filters_for_tcga()->JsonDictOfDictsOfScalarsModel:
+    ...
+
+get_filters_for_tcga.run()
