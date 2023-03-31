f6ca93a6c3a3d37789e6bbe6056b29ae6beb3207
Sveinung Gundersen <sveinugu@gmail.com>
2023-03-31 14:30:03 +0200

    Added TODO on runtime bug

--- src/omnipy/__init__.py
+++ src/omnipy/__init__.py
@@ -6,6 +6,9 @@ from typing import Optional
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# TODO: The check disabling runtime for tests also trigger for tests that are run outside of Omnipy, breaking
+#       tests on the user side. Find a better way to disable the global runtime object for Omnipy tests
+
 
 def _get_runtime() -> Optional['Runtime']:
     if 'pytest' not in sys.modules:
