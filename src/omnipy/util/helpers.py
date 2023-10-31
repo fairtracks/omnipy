8ccd6bae3cf6b56769af315189f826e1ceb6662b
Sveinung Gundersen <sveinugu@gmail.com>
2023-10-31 21:09:10 +0100

    Locale debug

--- src/omnipy/util/helpers.py
+++ src/omnipy/util/helpers.py
@@ -37,6 +37,7 @@ def remove_none_vals(**kwargs: object) -> Dict[object, object]:
 
 
 def get_datetime_format(locale: Optional[LocaleType] = None) -> str:
+    print(f'Setting locale to "{locale}"...')
     pkg_locale.setlocale(pkg_locale.LC_ALL, locale)
 
     if hasattr(pkg_locale, 'nl_langinfo'):  # noqa
