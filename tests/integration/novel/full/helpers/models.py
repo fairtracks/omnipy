9667928fe61afca1473f6782d4a9602888efe4df
Sveinung Gundersen <sveinugu@gmail.com>
2023-12-01 23:58:28 +0100

    Refactor RecordSchema classes for test. Documented various complex typing issues related to mypy, omnipy and pydantic + added TODO

--- tests/integration/novel/full/helpers/models.py
+++ tests/integration/novel/full/helpers/models.py
@@ -1,4 +1,5 @@
-from typing import Dict, Generic, List, Type, TypeVar, Union
+import typing
+from typing import Dict, Generic, List, Mapping, Type, TypeVar, Union
 
 from pydantic import BaseConfig, BaseModel, create_model, Extra
 
@@ -6,9 +7,70 @@ from omnipy.data.model import Model
 
 # Types
 
-RecordT = TypeVar('RecordT', bound=Model)
 
-RecordSchemaDefType = Dict[str, Type]
+class RecordSchemaBase(BaseModel):
+    class Config:
+        extra = Extra.forbid
+
+
+# TODO: Revisit RecordT typing with pydantic v2 and/or new versions of mypy and python
+#
+# A rundown of various typing-related issues with the RecordT TypeVar, for later reference.
+# Hours were spend figuring this out. Let's start with the following TypeVar definition, which
+# looks like it ought to be working:
+#
+#     RecordT = TypeVar(
+#         'RecordT', bound=Model[Union[Dict[str, Union[Type, UnionType]], RecordSchemaBase]])
+#
+# There are, however, several issues with this that cause mypy to complain, however the exact
+# reasons are difficult to unravel. Mypy only complains with a very generic error message:
+#
+#     Type argument "GeneralRecord" of "TableTemplate" must be a subtype of
+#     "Model[Union[Dict[str, Union[Type[Any], UnionType]], RecordSchemaBase]]"  [type-var] (95:25)
+#
+# The first reason for failure is that Dict is invariant, while `GeneralRecord` is a specialization
+# of the bound `RecordT` type variable and therefore needs it to be covariant or contravariant.
+# Switching `Dict` with `Mapping`, which is not invariant, still does not work:
+#
+#     RecordT = TypeVar(
+#         'RecordT', bound=Model[Union[Mapping[str, Union[Type, UnionType]], RecordSchemaBase]])
+#
+# Now the code fails not run at all with the error:
+#
+#     TypeError: Can't instantiate abstract class Mapping with abstract methods
+#                __getitem__, __iter__, __len__
+#
+# Which is due to omnipy's Mapping class, which tries to instantiate the type argument to generate
+# a default value. In case of a Union, the first type is the one that needs to be instantiated,
+# so a simple fix is to change the order:
+#
+#     RecordT = TypeVar(
+#         'RecordT', bound=Model[Union[RecordSchemaBase, Mapping[str, Union[Type, UnionType]]]])
+#
+# However, the omnipy-related error is now replaced with a Pydantic-related one (Pydantic v1):
+#
+#     TypeError: 'member_descriptor' object is not iterable
+#
+# In addition, mypy still fails, unhelpfully with the exact same error message as above. It turns
+# out the reason for both is that neither of Pydantic version 1.10.13 or mypy v1.7.0 supports
+# `UnionType` introduced in Python 3.10, even when `GeneralRecord` is defined using new union
+# notation (`int | str` instead of `Union[int, str]`). Both pydantic and mypy, however, works with
+# `typing._UnionGenericAlias` independently of notation type even though (in Python 3.10):
+#
+#     ```python
+#     isinstance(int | str,  _UnionGenericAlias) == False
+#     isinstance(int | str, UnionType) == True
+#     isinstance(Union[int, str],  _UnionGenericAlias) == False
+#     isinstance(Union[int, str],  UnionType) == True
+#     ```
+#
+# Due to this inconsistency and the use of a private type, `UnionType` was switched to `object`.
+# With the final variant, all of mypy, pydantic and omnipy work as they should:
+
+RecordT = TypeVar(
+    'RecordT', bound=Model[Union[RecordSchemaBase, Mapping[str, Union[Type, object]]]])
+
+RecordSchemaDefType = Dict[str, Type[object]]
 
 # Models
 
@@ -17,24 +79,47 @@ class RecordSchemaDef(Model[RecordSchemaDefType]):
     ...
 
 
-class RecordSchema(BaseModel):
-    ...
-
-
 def record_schema_factory(data_file: str,
-                          record_schema_def: RecordSchemaDefType) -> Type[RecordSchema]:
+                          record_schema_def: RecordSchemaDefType) -> Type[RecordSchemaBase]:
     class Config(BaseConfig):
         extra = Extra.forbid
 
     return create_model(
         data_file,
-        **dict(((key, (val, val())) for key, val in record_schema_def.items())),
-        __config__=Config)
+        __base__=RecordSchemaBase,
+        **{
+            key: (val, val()) for key, val in record_schema_def.items()
+        },
+    )
 
 
-MyRecordSchema = record_schema_factory('MyRecordSchema', {'a': int, 'b': str})
+if typing.TYPE_CHECKING:
+    # TODO: Revisit the need for static classes for mypy after updates to Pydantic v2
+    #       as this is a workaround only for these hard-coded test cases
 
-MyOtherRecordSchema = record_schema_factory('MyOtherRecordSchema', {'b': str, 'c': int})
+    class MyRecordSchemaBase(RecordSchemaBase):
+        a: int
+        b: str
+
+    class MyRecordSchema(Model[MyRecordSchemaBase]):
+        ...
+
+    class MyOtherRecordSchemaBase(RecordSchemaBase):
+        b: str
+        c: int
+
+    class MyOtherRecordSchemaModel(Model[MyOtherRecordSchemaBase]):
+        ...
+else:
+    MyRecordSchemaBase = record_schema_factory('MyRecordSchemaBase', {'a': int, 'b': str})
+
+    class MyRecordSchema(Model[MyRecordSchemaBase]):
+        ...
+
+    MyOtherRecordSchemaBase = record_schema_factory('MyOtherRecordSchemaBase', {'b': str, 'c': int})
+
+    class MyOtherRecordSchema(Model[MyOtherRecordSchemaBase]):
+        ...
 
 
 class TableTemplate(Model[List[RecordT]], Generic[RecordT]):
