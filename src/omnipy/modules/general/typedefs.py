1ed558297013a9a1c78b10f690057d1b5d173d98
Sveinung Gundersen <sveinugu@gmail.com>
2023-12-09 16:12:39 +0100

    Implemented FrozenDict

---
+++ src/omnipy/modules/general/typedefs.py
@@ -0,0 +1,58 @@
+from collections import UserDict
+from types import MappingProxyType
+from typing import Generic, Optional, Sequence, TypeVar
+
+_KeyT = TypeVar('_KeyT')
+_ValT = TypeVar('_ValT')
+
+
+# Unfortunately, MappingProxyType is declared as a final class, which means it cannot be subclassed.
+# Inheriting from UserDict is a workaround.
+class FrozenDict(UserDict[_KeyT, _ValT], Generic[_KeyT, _ValT]):
+    """
+    FrozenDict works exactly like a dict except that it cannot be modified after initialisation:
+
+        >>> from omnipy.modules.general.typedefs import FrozenDict
+        >>> from omnipy.util.helpers import print_exception
+        >>> from types import MappingProxyType
+        >>>
+        >>> my_dict = FrozenDict({'temperature': 'chilly'})
+        >>> my_dict
+        mappingproxy({'temperature': 'chilly'})
+        >>> my_dict['temperature']
+        'chilly'
+        >>> with print_exception:
+        ...     my_dict['temperature'] = 'scorching'
+        TypeError: 'mappingproxy' object does not support item assignment
+
+    In contrast to types.MappingProxyType, FrozenDict can be initialized without parameters:
+
+        >>> with print_exception:
+        ...     MappingProxyType()
+        TypeError: mappingproxy() missing required argument 'mapping' (pos 1)
+        >>> FrozenDict()
+        mappingproxy({})
+
+    Typically, this would be a rather useless thing to do, as the resulting object would be an empty
+    dictionary that cannot be changed. With Omnipy it actually makes sense, though, as the Model
+    class calls the type argument without parameters to determine the default value of the model.
+
+    Note:
+        Mutable objects within a FrozenDict are still mutable:
+
+        >>> my_dict = FrozenDict({'temp_map': {'cold': 'chilly', 'hot': 'scorching'}})
+        >>> my_dict['temp_map']['hot'] = 'blazing'
+        >>> my_dict
+        mappingproxy({'temp_map': {'cold': 'chilly', 'hot': 'blazing'}})
+
+        To implement a fully immutable nested data structure, one would need a mechanism that makes
+        sure only immutable types are allowed within a FrozenDict (or a tuple, which is the
+        immutable list counterpart). Luckily, Omnipy provides just that guarantee with
+        NestedFrozenDictsModel, NestedTuplesModel and NestedFrozenCollectionsModel.
+    """
+    def __init__(self,
+                 unfrozen_dict: Optional[dict[_KeyT, _ValT] | Sequence[tuple[_KeyT, _ValT]]] = None,
+                 /,
+                 **kwargs):
+        super().__init__(unfrozen_dict, **kwargs)
+        self.data: MappingProxyType[_KeyT, _ValT] = MappingProxyType(self.data)  # type: ignore
