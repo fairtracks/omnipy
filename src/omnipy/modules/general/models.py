470185b1f8734b9f9c871335095cd1ac4a97d0e3
Sveinung Gundersen <sveinugu@gmail.com>
2023-12-09 16:29:13 +0100

    NotIteratorExceptStrings -> NotIterableExceptStrOrBytesModel plus tests

--- src/omnipy/modules/general/models.py
+++ src/omnipy/modules/general/models.py
@@ -1,8 +1,32 @@
-from collections.abc import Iterable
+from typing import Optional
 
 from omnipy.data.model import Model
+from omnipy.util.helpers import is_iterable
 
 
-class NotIteratorExceptStrings(Model[object]):
+class NotIterableExceptStrOrBytesModel(Model[Optional[object]]):
+    """
+    Model describing any object that is not iterable, except for `str` and `bytes` types.
+    As strings and bytes are iterable (over the characters/bytes) but also generally useful and
+    often considered singular (or scalar) types, they are specifically allowed by this model.
+
+    Examples:
+        >>> from omnipy import NotIterableExceptStrOrBytesModel, print_exception
+        >>>
+        >>> NotIterableExceptStrOrBytesModel(1234)
+        NotIterableExceptStrOrBytesModel(1234)
+        >>> NotIterableExceptStrOrBytesModel('1234')
+        NotIterableExceptStrOrBytesModel(1234)
+        >>> with print_exception:
+        ...     NotIterableExceptStrOrBytesModel((1, 2, 3, 4))
+        ValidationError: 1 validation error for NotIterableExceptStrOrBytesModel
+
+    Note:
+        JsonScalarModel is a strict submodel of NotIterableExceptStrOrBytesModel in that all objects
+        allowed by JsonScalarModel are also allowed by NotIterableExceptStrOrBytesModel.
+    """
+    @classmethod
     def _parse_data(cls, data: object) -> object:
-        assert isinstance(data, str) or not isinstance(data, Iterable)
+        assert isinstance(data, str) or isinstance(data, bytes) or not is_iterable(data), \
+            f'Data of type {type(data)} is iterable'
+        return data
