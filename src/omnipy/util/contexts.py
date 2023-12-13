c29d43d9ec46ea50f5396396db9ff5d46acd0b60
Sveinung Gundersen <sveinugu@gmail.com>
2023-12-13 21:33:11 +0100

    Added support in AttribHolder for resetting to other value. Refactored and cleaned up

--- src/omnipy/util/contexts.py
+++ src/omnipy/util/contexts.py
@@ -35,37 +35,55 @@ class AttribHolder(AbstractContextManager):
     def __init__(self,
                  obj: object,
                  attr_name: str,
-                 other_attr: object = None,
+                 other_value: object = Undefined,
+                 reset_to_other: bool = False,
+                 switch_to_other: bool = False,
                  on_class: bool = False,
                  copy_attr: bool = False):
         self._obj_or_cls = obj.__class__ if on_class else obj
         self._attr_name = attr_name
-        self._other_attr = other_attr
-        self._prev_attr: object | None = None
+        self._other_value = None if other_value is Undefined else other_value
+        self._prev_value: object | None = None
+        self._reset_to_other = reset_to_other
+        self._switch_to_other = switch_to_other
         self._copy_attr = copy_attr
         self._store_prev_attr = False
         self._set_attr_to_other = False
 
+        assert not (reset_to_other and switch_to_other), \
+            'Only one of `reset_to_other` and `switch_to_other` can be specified.'
+
+        if other_value is not Undefined:
+            assert reset_to_other or switch_to_other, \
+                ('If other_value is specified, you also need to set one of `reset_to_other` and '
+                 '`switch_to_other`')
+
+        if reset_to_other or switch_to_other:
+            assert other_value is not Undefined, \
+                ('If one of `reset_to_other` and `switch_to_other` are specified, you also need '
+                 'to provide a value for `other_value`')
+
     def __enter__(self):
         if hasattr(self._obj_or_cls, self._attr_name):
-            attr = getattr(self._obj_or_cls, self._attr_name)
+            attr_value = getattr(self._obj_or_cls, self._attr_name)
 
-            self._set_attr_to_other = self._other_attr is not None and self._other_attr != attr
-            self._store_prev_attr = self._other_attr is None or self._set_attr_to_other
+            self._store_prev_attr = \
+                not self._reset_to_other and not all_equals(attr_value, self._other_value)
 
             if self._store_prev_attr:
-                self._prev_attr = copy(attr) if self._copy_attr else attr
+                self._prev_value = deepcopy(attr_value) if self._copy_attr else attr_value
 
-            if self._set_attr_to_other:
-                setattr(self._obj_or_cls, self._attr_name, self._other_attr)
+            if self._switch_to_other:
+                setattr(self._obj_or_cls, self._attr_name, self._other_value)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self._set_attr_to_other or exc_val is not None:
-            setattr(self._obj_or_cls, self._attr_name, self._prev_attr)
+        if self._switch_to_other or exc_val is not None:
+            reset_value = self._other_value if self._reset_to_other else self._prev_value
+            setattr(self._obj_or_cls, self._attr_name, reset_value)
 
         if self._store_prev_attr:
-            self._prev_attr = None
+            self._prev_value = None
 
 
 class PrintExceptionContext(AbstractContextManager):
--- tests/util/test_contexts.py
+++ tests/util/test_contexts.py
@@ -65,6 +65,26 @@ def test_with_last_error() -> None:
     assert 'a=4 is even' in str(exc_info.getrepr())
 
 
+def test_attrib_holder_init() -> None:
+    class A:
+        def __init__(self, num: int) -> None:
+            self.num = num
+
+    a = A(5)
+
+    with pytest.raises(AssertionError):
+        AttribHolder(a, 'num', reset_to_other=True)
+
+    with pytest.raises(AssertionError):
+        AttribHolder(a, 'num', switch_to_other=True)
+
+    with pytest.raises(AssertionError):
+        AttribHolder(a, 'num', 9)
+
+    with pytest.raises(AssertionError):
+        AttribHolder(a, 'num', 9, reset_to_other=True, switch_to_other=True)
+
+
 def test_with_class_attrib_holder_reset_attr_if_exception() -> None:
     class A:
         ...
@@ -75,51 +95,69 @@ def test_with_class_attrib_holder_reset_attr_if_exception() -> None:
 
     a = A()
     with AttribHolder(a, 'num') as ms:
-        assert ms._prev_attr is None
+        assert ms._prev_value is None
 
     b = B(5)
     with AttribHolder(b, 'num') as ms:
         b.num = 7
-        assert ms._prev_attr == 5
+        assert ms._prev_value == 5
     assert b.num == 7
 
     try:
         b.num = 5
         with AttribHolder(b, 'num') as ms:
             b.num = 7
-            assert ms._prev_attr == 5
+            assert ms._prev_value == 5
             raise RuntimeError()
     except RuntimeError:
         pass
     assert b.num == 5
 
 
-def test_with_class_attrib_holder_reset_attr_if_exception_copy() -> None:
+def test_with_class_attrib_holder_set_attr_to_other_if_exception() -> None:
+    class A:
+        def __init__(self, num: int) -> None:
+            self.num = num
+
+    a = A(5)
+
+    try:
+        a.num = 5
+        with AttribHolder(a, 'num', 9, reset_to_other=True) as ms:
+            a.num = 7
+            assert ms._prev_value is None
+            raise RuntimeError()
+    except RuntimeError:
+        pass
+    assert a.num == 9
+
+
+def test_with_class_attrib_holder_reset_attr_if_exception_deepcopy() -> None:
     class B:
-        def __init__(self, numbers: list[int]) -> None:
+        def __init__(self, numbers: list[list[int]]) -> None:
             self.numbers = numbers
 
-    b = B([5])
+    b = B([[5]])
     try:
         with AttribHolder(b, 'numbers') as ms:
-            b.numbers[0] += 2
-            assert b.numbers == [7]
-            assert ms._prev_attr == [7]
+            b.numbers[0][0] += 2
+            assert b.numbers == [[7]]
+            assert ms._prev_value == [[7]]
             raise RuntimeError()
     except RuntimeError:
         pass
-    assert b.numbers == [7]
+    assert b.numbers == [[7]]
 
     try:
-        b.numbers = [5]
+        b.numbers = [[5]]
         with AttribHolder(b, 'numbers', copy_attr=True) as ms:
-            b.numbers[0] += 2
-            assert b.numbers == [7]
-            assert ms._prev_attr == [5]
+            b.numbers[0][0] += 2
+            assert b.numbers == [[7]]
+            assert ms._prev_value == [[5]]
             raise RuntimeError()
     except RuntimeError:
         pass
-    assert b.numbers == [5]
+    assert b.numbers == [[5]]
 
 
 def test_with_class_attrib_holder_method_switching() -> None:
@@ -134,29 +172,29 @@ def test_with_class_attrib_holder_method_switching() -> None:
         return 'other_method'
 
     a = A()
-    with AttribHolder(a, 'method', other_method, on_class=True) as ms:
-        assert ms._prev_attr is None
+    with AttribHolder(a, 'method', other_method, switch_to_other=True, on_class=True) as ms:
+        assert ms._prev_value is None
         with pytest.raises(AttributeError):
             a.method()  # type: ignore
 
     A.method = other_method
 
     a = A()
-    with AttribHolder(a, 'method', other_method, on_class=True) as ms:
+    with AttribHolder(a, 'method', other_method, switch_to_other=True, on_class=True) as ms:
         assert a.method() == 'other_method'  # type: ignore
-        assert ms._prev_attr is None
+        assert ms._prev_value is None
 
     b = B()
-    with AttribHolder(b, 'method', other_method, on_class=True) as ms:
+    with AttribHolder(b, 'method', other_method, switch_to_other=True, on_class=True) as ms:
         assert b.method() == 'other_method'
-        assert ms._prev_attr.__name__ == 'method'
+        assert ms._prev_value.__name__ == 'method'
     assert b.method() == 'method'
 
     b = B()
     try:
-        with AttribHolder(b, 'method', other_method, on_class=True) as ms:
+        with AttribHolder(b, 'method', other_method, switch_to_other=True, on_class=True) as ms:
             assert b.method() == 'other_method'
-            assert ms._prev_attr.__name__ == 'method'
+            assert ms._prev_value.__name__ == 'method'
             raise RuntimeError()
     except RuntimeError:
         pass
