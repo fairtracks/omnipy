bb94d5e819cb75682f75b15e4d208934b76de69c
Sveinung Gundersen <sveinugu@gmail.com>
2023-12-13 21:31:40 +0100

    Updated add_callback_after_call decorator to allow adding extra context

--- src/omnipy/util/decorators.py
+++ src/omnipy/util/decorators.py
@@ -1,3 +1,4 @@
+from contextlib import AbstractContextManager
 from typing import Callable, ParamSpec, TypeVar
 
 DecoratedP = ParamSpec('DecoratedP')
@@ -8,16 +9,24 @@ CallbackP = ParamSpec('CallbackP')
 def add_callback_after_call(func: Callable[DecoratedP, DecoratedR],
                             callback_func: Callable[CallbackP, None],
                             *cb_args: CallbackP.args,
+                            with_context: AbstractContextManager | None = None,
                             **cb_kwargs: CallbackP.kwargs) -> Callable[DecoratedP, DecoratedR]:
-    class ValidateAfterCall:
+    class ValidateAfterCall(AbstractContextManager):
         def __enter__(self):
             ...
 
         def __exit__(self, exc_type, exc_val, exc_tb):
+            ret = None
+
             if exc_val is None:
                 callback_func(*cb_args, **cb_kwargs)
+            return ret
 
     def _inner(*args: DecoratedP.args, **kwargs: DecoratedP.kwargs) -> DecoratedR:
+        if with_context:
+            with with_context:
+                with ValidateAfterCall():
+                    return func(*args, **kwargs)
         with ValidateAfterCall():
             return func(*args, **kwargs)
 
