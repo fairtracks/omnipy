397f87931d12617bebc60f712add930f4b63e5a2
Sveinung Gundersen <sveinugu@gmail.com>
2024-01-06 01:44:01 +0100

    Implemented parametrized Models for line/column split/join

--- src/omnipy/modules/raw/models.py
+++ src/omnipy/modules/raw/models.py
@@ -1,20 +1,38 @@
 import os
 
-from omnipy.data.model import Model
+from omnipy.data.model import ListOfParamModel, Model, ParamModel
 
 
-class SplitLinesModel(Model[str | list[str]]):
+class BytesModel(ParamModel[str | bytes, str]):
     @classmethod
-    def _parse_data(cls, data: str | list[str]) -> list[str]:
+    def _parse_data(cls, data: str | bytes, encoding: str = 'utf-8') -> bytes:
+        if isinstance(data, bytes):
+            return data
+
+        return data.encode(encoding)
+
+    ...
+
+
+class StrModel(ParamModel[bytes | str, str]):
+    @classmethod
+    def _parse_data(cls, data: bytes | str, encoding: str = 'utf-8') -> str:
+        if isinstance(data, str):
+            return data
+
+        return data.decode(encoding)
+
+    ...
+
+
+class SplitToLinesModel(ParamModel[str | list[str], bool]):
+    @classmethod
+    def _parse_data(cls, data: str | list[str], strip: bool = True) -> list[str]:
         if isinstance(data, list):
             return data
-        return data.split(os.linesep)
 
-
-class SplitAndStripLinesModel(SplitLinesModel):
-    @classmethod
-    def _parse_data(cls, data: str | list[str]) -> list[str]:
-        return [line.strip() for line in super()._parse_data(data)]
+        lines = data.split(os.linesep)
+        return [line.strip() for line in lines] if strip else lines
 
 
 class JoinLinesModel(Model[list[str] | str]):
@@ -23,3 +41,33 @@ class JoinLinesModel(Model[list[str] | str]):
         if isinstance(data, str):
             return data
         return os.linesep.join(data)
+
+
+class SplitToItemsModel(ParamModel[str | list[str], bool | str]):
+    @classmethod
+    def _parse_data(cls,
+                    data: str | list[str],
+                    strip: bool = True,
+                    delimiter: str = '\t') -> list[str]:
+        if isinstance(data, list):
+            return data
+
+        items = data.split(delimiter)
+        return [item.strip() for item in items] if strip else items
+
+
+class JoinItemsModel(ParamModel[list[str] | str, str]):
+    @classmethod
+    def _parse_data(cls, data: list[str] | str, delimiter: str = '\t') -> str:
+        if isinstance(data, str):
+            return data
+
+        return delimiter.join(data)
+
+
+class SplitLinesToColumnsModel(ListOfParamModel[SplitToItemsModel, bool | str]):
+    ...
+
+
+class JoinColumnsToLinesModel(ListOfParamModel[JoinItemsModel, str]):
+    ...
--- tests/modules/raw/test_models.py
+++ tests/modules/raw/test_models.py
@@ -1,10 +1,47 @@
 import os
 from textwrap import dedent
 
-from omnipy.modules.raw.models import JoinLinesModel, SplitAndStripLinesModel, SplitLinesModel
+from pydantic import ValidationError
+import pytest
+
+from omnipy.modules.raw.models import (BytesModel,
+                                       JoinColumnsToLinesModel,
+                                       JoinItemsModel,
+                                       JoinLinesModel,
+                                       SplitLinesToColumnsModel,
+                                       SplitToItemsModel,
+                                       SplitToLinesModel,
+                                       StrModel)
 
 
-def test_split_and_join_lines_models():
+def test_bytes_model():
+    assert BytesModel(b'').contents == b''
+    assert BytesModel(b'\xc3\xa6\xc3\xb8\xc3\xa5').contents == b'\xc3\xa6\xc3\xb8\xc3\xa5'
+    assert BytesModel('').contents == b''
+    assert BytesModel('æøå').contents == b'\xc3\xa6\xc3\xb8\xc3\xa5'
+    assert BytesModel('æøå', encoding='utf-8').contents == b'\xc3\xa6\xc3\xb8\xc3\xa5'
+    assert BytesModel('æøå', encoding='latin-1').contents == b'\xe6\xf8\xe5'
+
+    with pytest.raises(LookupError):
+        BytesModel('æøå', encoding='my-encoding')
+
+
+def test_str_model():
+    assert StrModel('').contents == ''
+    assert StrModel('æøå').contents == 'æøå'
+    assert StrModel(b'').contents == ''
+    assert StrModel(b'\xc3\xa6\xc3\xb8\xc3\xa5').contents == 'æøå'
+    assert StrModel(b'\xc3\xa6\xc3\xb8\xc3\xa5', encoding='utf-8').contents == 'æøå'
+    assert StrModel(b'\xe6\xf8\xe5', encoding='latin-1').contents == 'æøå'
+
+    with pytest.raises(ValidationError):
+        StrModel(b'\xe6\xf8\xe5', encoding='utf-8')
+
+    with pytest.raises(LookupError):
+        StrModel(b'\xe6\xf8\xe5', encoding='my-encoding')
+
+
+def test_split_to_and_join_lines_models():
     data = """\
         Alas, poor Yorick! I knew him, Horatio: a fellow
         of infinite jest, of most excellent fancy: he hath
@@ -16,11 +53,11 @@ def test_split_and_join_lines_models():
         that were wont to set the table on a roar? Not one
         now, to mock your own grinning? quite chap-fallen."""
 
-    lines_unstripped = SplitLinesModel(data)
-    assert lines_unstripped[0] == '        Alas, poor Yorick! I knew him, Horatio: a fellow'
-
-    lines_stripped = SplitAndStripLinesModel(data)
+    lines_stripped = SplitToLinesModel(data)
     assert lines_stripped[0] == 'Alas, poor Yorick! I knew him, Horatio: a fellow'
+
+    lines_unstripped = SplitToLinesModel(data, strip=False)
+    assert lines_unstripped[0] == '        Alas, poor Yorick! I knew him, Horatio: a fellow'
 
     last_lines = lines_stripped[3:]
     assert last_lines[0:2].contents == [
@@ -34,7 +71,74 @@ def test_split_and_join_lines_models():
         it. Here hung those lips that I have kissed I know""")
 
     assert joined_lines[:joined_lines.index(' ')].contents == 'abhorred'
-
-    assert JoinLinesModel(SplitLinesModel(data)).contents == data
-    assert JoinLinesModel(SplitAndStripLinesModel(data)).contents == \
+    assert JoinLinesModel(SplitToLinesModel(data)).contents == \
            os.linesep.join([line.strip() for line in data.split(os.linesep)])
+
+    assert JoinLinesModel(SplitToLinesModel(data, strip=False)).contents == data
+
+
+def test_split_to_and_join_items() -> None:
+    data_tab = "abc\t def \tghi\tjkl"
+
+    items_stripped_tab = SplitToItemsModel(data_tab)
+    assert items_stripped_tab.contents == ['abc', 'def', 'ghi', 'jkl']
+    assert items_stripped_tab[1] == 'def'
+    assert items_stripped_tab[-2:].contents == ['ghi', 'jkl']
+
+    items_unstripped_tab = SplitToItemsModel(data_tab, strip=False)
+    assert items_unstripped_tab.contents == ['abc', ' def ', 'ghi', 'jkl']
+    assert items_unstripped_tab[1] == ' def '
+    assert items_unstripped_tab[-2:].contents == ['ghi', 'jkl']
+
+    data_comma = "abc, def, ghi, jkl"
+
+    items_stripped_comma = SplitToItemsModel(data_comma, delimiter=',')
+    assert items_stripped_comma.contents == ['abc', 'def', 'ghi', 'jkl']
+    assert items_stripped_comma[1] == 'def'
+    assert items_stripped_comma[-2:].contents == ['ghi', 'jkl']
+
+    tab_joined_items = JoinItemsModel(items_stripped_tab[1:3])
+    assert tab_joined_items.contents == 'def\tghi'
+    assert tab_joined_items[1:-1].contents == 'ef\tgh'
+
+    comma_space_joined_items = JoinItemsModel(items_stripped_comma[1:3], delimiter=', ')
+    assert comma_space_joined_items.contents == 'def, ghi'
+    assert comma_space_joined_items[1:-1].contents == 'ef, gh'
+
+
+def test_split_lines_to_columns_and_join_columns_to_lines() -> None:
+    data_tab = ['abc\t def \tghi\t jkl', 'mno\t pqr\tstu\t vwx', 'yz']
+
+    cols_stripped_tab = SplitLinesToColumnsModel(data_tab)
+    assert cols_stripped_tab[0].contents == ['abc', 'def', 'ghi', 'jkl']
+    assert cols_stripped_tab[0][1] == 'def'
+    assert cols_stripped_tab[1:2].contents == [SplitToItemsModel('mno\t pqr\tstu\t vwx')]
+    assert cols_stripped_tab[1:].to_data() == [['mno', 'pqr', 'stu', 'vwx'], ['yz']]
+
+    cols_unstripped_tab = SplitLinesToColumnsModel(data_tab, strip=False)
+    assert cols_unstripped_tab[0].contents == ['abc', ' def ', 'ghi', ' jkl']
+    assert cols_unstripped_tab[0][1] == ' def '
+    assert cols_unstripped_tab[1:2].contents \
+           == [SplitToItemsModel('mno\t pqr\tstu\t vwx', strip=False)]
+    assert cols_unstripped_tab[1:].to_data() == [['mno', ' pqr', 'stu', ' vwx'], ['yz']]
+
+    data_comma = ['abc, def, ghi, jkl', 'mno, pqr, stu, vwx', 'yz']
+
+    cols_stripped_comma = SplitLinesToColumnsModel(data_comma, delimiter=',')
+    assert cols_stripped_comma[0].contents == ['abc', 'def', 'ghi', 'jkl']
+    assert cols_stripped_comma[0][1] == 'def'
+    assert cols_stripped_comma[1:2].contents \
+           == [SplitToItemsModel('mno, pqr, stu, vwx', delimiter=',')]
+    assert cols_stripped_comma[1:].to_data() == [['mno', 'pqr', 'stu', 'vwx'], ['yz']]
+
+    joined_cols = JoinColumnsToLinesModel(cols_stripped_tab[1:])
+    assert joined_cols.contents == [JoinItemsModel('mno\tpqr\tstu\tvwx'), \
+                                    JoinItemsModel('yz')]
+    assert joined_cols[1:].contents == [JoinItemsModel('yz')]
+    assert joined_cols.to_data() == ['mno\tpqr\tstu\tvwx', 'yz']
+
+    joined_cols = JoinColumnsToLinesModel(cols_stripped_comma[1:], delimiter=', ')
+    assert joined_cols.contents == [JoinItemsModel('mno, pqr, stu, vwx'), \
+                                    JoinItemsModel('yz')]
+    assert joined_cols[1:].contents == [JoinItemsModel('yz')]
+    assert joined_cols.to_data() == ['mno, pqr, stu, vwx', 'yz']
