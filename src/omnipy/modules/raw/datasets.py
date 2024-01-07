ad09005604027c0b22d303e66db6ad0a86cd50d1
Sveinung Gundersen <sveinugu@gmail.com>
2024-01-07 12:41:36 +0100

    Implement raw dataset types

---
+++ src/omnipy/modules/raw/datasets.py
@@ -0,0 +1,41 @@
+from omnipy.data.dataset import Dataset, ListOfParamModelDataset, ParamDataset
+from omnipy.modules.raw.models import (BytesModel,
+                                       JoinColumnsToLinesModel,
+                                       JoinItemsModel,
+                                       JoinLinesModel,
+                                       SplitLinesToColumnsModel,
+                                       SplitToItemsModel,
+                                       SplitToLinesModel,
+                                       StrModel)
+
+
+class BytesDataset(ParamDataset[BytesModel, str]):
+    ...
+
+
+class StrDataset(ParamDataset[StrModel, str]):
+    ...
+
+
+class SplitToLinesDataset(ParamDataset[SplitToLinesModel, bool]):
+    ...
+
+
+class JoinLinesDataset(Dataset[JoinLinesModel]):
+    ...
+
+
+class SplitToItemsDataset(ParamDataset[SplitToItemsModel, bool | str]):
+    ...
+
+
+class JoinItemsDataset(ParamDataset[JoinItemsModel, str]):
+    ...
+
+
+class SplitLinesToColumnsDataset(ListOfParamModelDataset[SplitLinesToColumnsModel, bool | str]):
+    ...
+
+
+class JoinColumnsToLinesDataset(ListOfParamModelDataset[JoinColumnsToLinesModel, str]):
+    ...
