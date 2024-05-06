50fb8e0acb6a8244427681853f4f8708d2c83046
Sveinung Gundersen <sveinugu@gmail.com>
2024-05-06 12:49:57 +0200

    Fixed typing of TableWithColNamesModel

--- src/omnipy/modules/tables/models.py
+++ src/omnipy/modules/tables/models.py
@@ -1,4 +1,4 @@
-from typing import Generic, TypeVar
+from typing import cast, Generic, TypeVar
 
 from pydantic import BaseModel
 
@@ -9,6 +9,7 @@ from ..json.models import (JsonCustomListModel,
                            JsonListM,
                            JsonListOfDictsOfScalarsModel,
                            JsonListOfListsOfScalarsModel)
+from ..json.typedefs import JsonListOfDictsOfScalars
 
 
 class TableOfStrings(Model[list[dict[str, str]]]):
@@ -23,30 +24,38 @@ class TableOfStringsAndLists(Model[list[dict[str, str | list[str]]]]):
     ...
 
 
-class TableWithColNamesModel(Model[JsonListOfListsOfScalarsModel | JsonListOfDictsOfScalarsModel]):
+class TableWithColNamesModel(Model[JsonListOfDictsOfScalarsModel | JsonListOfListsOfScalarsModel]):
     @classmethod
-    def _parse_data(cls, data: JsonListOfListsOfScalarsModel | JsonListOfDictsOfScalarsModel):
+    def _parse_data(  # type: ignore[override]
+        cls,
+        data: JsonListOfDictsOfScalarsModel | JsonListOfListsOfScalarsModel
+    ) -> JsonListOfDictsOfScalarsModel | JsonListOfDictsOfScalars:
         if len(data) > 0:
-            if isinstance(data[0], JsonListM):
-                first_row_as_colnames = JsonCustomListModel[str](data[0])
-                rows = data[1:]
+            if isinstance(data[0], JsonListM):  # type: ignore[index]
+                first_row_as_colnames = JsonCustomListModel[str](data[0])  # type: ignore[index]
+                first_row_as_colnames_data: list[str] = \
+                    first_row_as_colnames.to_data()  # type: ignore[assignment]
+                rows = data[1:]  # type: ignore[index]
+
                 # if len(rows) == 0:
                 #     rows = [[None] * len(first_row_as_colnames)]
+
+                # col_name: JsonListOfScalars
                 return [{
                     col_name: row[i] if i < len(row) else None for i,
-                    col_name in enumerate(first_row_as_colnames)
+                    col_name in enumerate(first_row_as_colnames_data)
                 } for row in rows]
             else:
-                assert isinstance(data[0], JsonDictM)
-                return data
+                assert isinstance(data[0], JsonDictM)  # type: ignore[index]
+                return cast(JsonListOfDictsOfScalarsModel, data)
 
-        return data
+        return cast(JsonListOfDictsOfScalarsModel, data)
 
     @property
     def col_names(self) -> tuple[str]:
         col_names = {}
         for row in self:
-            col_names.update(dict.fromkeys(row.keys()))
+            col_names.update(dict.fromkeys(row.keys()))  # type: ignore[attr-defined]
         return tuple(col_names.keys())
 
 
