69eab1274ae0c0b9fe49b267a0b6ee45c8db6024
Sveinung Gundersen <sveinugu@gmail.com>
2024-08-18 14:07:52 +0200

    Reimplementation of TableOfPydanticRecordsModel for 'bed' demo

--- src/omnipy/modules/tables/models.py
+++ src/omnipy/modules/tables/models.py
@@ -1,15 +1,17 @@
-from typing import cast, Generic, TypeVar
+from typing import cast, Generic, get_args, TypeVar
 
 from pydantic import BaseModel
 
 from omnipy.data.model import Model
 
+from ..general.models import Chain3
 from ..json.models import (JsonCustomDictModel,
                            JsonCustomListModel,
                            JsonListOfDictsOfScalarsModel,
                            JsonListOfListsOfScalarsModel,
                            JsonListOfScalarsModel)
 from ..json.typedefs import JsonListOfDictsOfScalars
+from ..raw.models import SplitLinesToColumnsModel, SplitToLinesModel
 
 # class TableOfStrings(Model[list[dict[str, str]]]):
 #     ...
@@ -23,11 +25,11 @@ from ..json.typedefs import JsonListOfDictsOfScalars
 #     ...
 
 
+# TODO: Add a test for TableWithColNamesModel
 class TableWithColNamesModel(Model[JsonListOfDictsOfScalarsModel | JsonListOfListsOfScalarsModel]):
     @classmethod
-    def _parse_data(  # type: ignore[override]
-        cls,
-        data: JsonListOfDictsOfScalarsModel | JsonListOfListsOfScalarsModel
+    def _parse_data(
+        cls, data: JsonListOfDictsOfScalarsModel | JsonListOfListsOfScalarsModel
     ) -> JsonListOfDictsOfScalarsModel | JsonListOfDictsOfScalars:
         if len(data) > 0:
             if isinstance(data[0].contents, list):
@@ -61,5 +63,24 @@ class TableWithColNamesModel(Model[JsonListOfDictsOfScalarsModel | JsonListOfLis
 _PydanticModelT = TypeVar('_PydanticModelT', bound=BaseModel)
 
 
-class TableOfPydanticRecordsModel(Model[list[_PydanticModelT]], Generic[_PydanticModelT]):
+class PydanticRecordModel(Model[_PydanticModelT | JsonListOfScalarsModel],
+                          Generic[_PydanticModelT]):
+    @classmethod
+    def _parse_data(cls, data: _PydanticModelT | JsonListOfScalarsModel) -> _PydanticModelT:
+        match data:
+            case JsonListOfScalarsModel():
+                pydantic_model = get_args(cls.outer_type(with_args=True))[0]
+                headers = pydantic_model.__fields__.keys()
+                assert len(headers) == len(data), f'{len(headers)} != {len(data)}'
+                return pydantic_model(**dict(zip(headers, data)))
+            case _:
+                return data
+
+
+class TableOfPydanticRecordsModel(
+        Model[list[PydanticRecordModel[_PydanticModelT]]
+              | Chain3[SplitToLinesModel,
+                       SplitLinesToColumnsModel,
+                       Model[list[PydanticRecordModel[_PydanticModelT]]]]],
+        Generic[_PydanticModelT]):
     ...
