191abf85d772cac6c577a4cd5e598a84199a8647
Sveinung Gundersen <sveinugu@gmail.com>
2024-09-24 13:42:10 +0200

    Improved speed of uniprot and other JSON-heavy operations by removing unneeded model creation

--- tests/modules/general/cases/raw/examples.py
+++ tests/modules/general/cases/raw/examples.py
@@ -8,12 +8,14 @@ f_complex = 2 + 3j
 f_list = [f_none, f_int, f_str, f_complex]
 f_dict = {'a': f_none, 'b': f_int, 'c': f_str, 'd': f_complex}
 f_tuple = (f_none, f_int, f_str, f_complex)
+f_empty_tuple = ()
 ej_tuple_iterable_scalar = (f_int, f_str, {})
 ej_tuple_wrong_scalar = (f_int, f_str, ej_type)
 f_frozendict = FrozenDict({'a': f_none, 1: f_int, 1 + 2j: f_str, 'd': f_complex})
 ej_frozendict_iter_scalar = {'a': f_none, 1: f_int, 1 + 2j: f_str, 'd': (1, 2)}
 ej_frozendict_iter_scalar_empty = {'a': f_none, 1: f_int, 1 + 2j: f_str, 'd': ()}
 ej_frozendict_wrong_scalar = {'a': f_none, 'b': f_int, 'c': f_str, 'd': ej_type}
+f_empty_frozendict = FrozenDict()
 f_set = {f_none, f_int, f_str, f_complex}
 
 e_int_key_dict = {f_int: f_none}
