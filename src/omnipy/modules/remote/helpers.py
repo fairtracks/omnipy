f97cd0f50eec4500d6256f82f98340889a4affee
Sveinung Gundersen <sveinugu@gmail.com>
2024-11-03 12:02:24 +0100

    Added automatic reset of rate limiter counting and delays for subsequent batches of requests

--- src/omnipy/modules/remote/helpers.py
+++ src/omnipy/modules/remote/helpers.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 from typing import Any, cast, Coroutine
 
 from aiohttp import ClientSession, TraceConfig
@@ -9,6 +10,10 @@ DEFAULT_TIME_PERIOD_IN_SECS = 60
 
 
 class RateLimitingClientSession(ClientSession):
+    """
+    A ClientSession that limits the number of requests made per time period, allowing an initial
+    burst of requests to go through before rate limiting kicks in for the rest.
+    """
     def __init__(self,
                  requests_per_time_period: float = DEFAULT_REQUESTS_PER_TIME_PERIOD,
                  time_period_in_secs: float = DEFAULT_TIME_PERIOD_IN_SECS,
@@ -29,13 +34,26 @@ class RateLimitingClientSession(ClientSession):
         self._limiter.max_rate = self._burst_size
         self._cur_delay_secs: float = 0
         self._num_requests: int = 0
+        self._first_submit_time: datetime | None = None
 
     async def _limit_request(self, *args, **kwargs):
 
         request_num = self._num_requests
         self._num_requests += 1
 
+        submit_time = datetime.now()
+        if self._first_submit_time is None:
+            self._first_submit_time = submit_time
+
         await self._limiter.acquire()
+
+        submit_time_delta = (submit_time - self._first_submit_time).total_seconds()
+        if submit_time_delta > self._time_period_in_secs:
+            # Resetting the rate limiter for the next batch of requests
+            self._first_submit_time = submit_time
+            self._cur_delay_secs = 0
+            self._num_requests = 1
+            request_num = 0
 
         # Adding a delay to compensate for the filling of the leaky bucket, which is not accounted
         # for in the AsyncLimiter. This is to allow for an initial burst of requests to go through
--- tests/modules/remote/test_rate_limiting.py
+++ tests/modules/remote/test_rate_limiting.py
@@ -26,34 +26,28 @@ async def my_endpoint_url(aiohttp_server) -> AsyncGenerator[str, None]:
 
 
 async def _assert_requests_and_get_run_time_in_secs(
+    client_session: RateLimitingClientSession,
     my_endpoint_url: str,
     num_requests: int,
-    requests_per_time_period: float,
-    time_period_in_secs: float,
 ) -> float:
-    client_session = RateLimitingClientSession(requests_per_time_period, time_period_in_secs)
-    async with client_session:
-        assert client_session.requests_per_second == requests_per_time_period / time_period_in_secs
-        assert isinstance(client_session, ClientSession)
+    start_time = datetime.now()
 
-        start_time = datetime.now()
-        async with client_session:
-            tasks = [client_session.get(my_endpoint_url) for _ in range(num_requests)]
-            responses = await asyncio.gather(*tasks)
-            for response in responses:
-                assert response.status == 200
-                assert await response.text() == '"My response"'
-        end_time = datetime.now()
-    return (end_time - start_time).total_seconds()
+    tasks = [client_session.get(my_endpoint_url) for _ in range(num_requests)]
+    responses = await asyncio.gather(*tasks)
+    for response in responses:
+        assert response.status == 200
+        assert await response.text() == '"My response"'
+
+    return (datetime.now() - start_time).total_seconds()
 
 
 @pytest.mark.parametrize(
     'num_requests, requests_per_time_period, time_period_in_secs, run_time_min, run_time_max',
     [
-        (21, 10, 0.5, 1, 1.1),
-        (21, 1, 0.05, 1, 1.1),
+        (21, 1, 0.05, 1, 1.07),
+        (21, 10, 0.5, 1, 1.05),
     ],
-    ids=['10_requests_per_0.5s', '1_requests_per_0.05s'],
+    ids=['no_burst_1_requests_per_0.05s', 'burst_10_requests_per_0.5s'],
 )
 async def test_rate_limiting_client_session(
     skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture],
@@ -64,10 +58,19 @@ async def test_rate_limiting_client_session(
     run_time_min: float,
     run_time_max: float,
 ) -> None:
-    run_time = await _assert_requests_and_get_run_time_in_secs(
-        my_endpoint_url,
-        num_requests,
-        requests_per_time_period,
-        time_period_in_secs,
-    )
-    assert run_time_min < run_time < run_time_max
+    client_session = RateLimitingClientSession(requests_per_time_period, time_period_in_secs)
+
+    async with client_session:
+        assert client_session.requests_per_second == requests_per_time_period / time_period_in_secs
+        assert isinstance(client_session, ClientSession)
+
+        for i in range(2):
+            run_time = await _assert_requests_and_get_run_time_in_secs(
+                client_session,
+                my_endpoint_url,
+                num_requests,
+            )
+
+            assert run_time_min < run_time < run_time_max
+
+            await asyncio.sleep(0.1)
