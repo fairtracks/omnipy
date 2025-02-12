ef7cc023ba8248a01de4fb256a1b6c450352dee7
Sveinung Gundersen <sveinugu@gmail.com>
2025-02-12 17:27:00 +0100

    Implemented OutputConfig

--- tests/data/test_display.py
+++ tests/data/test_display.py
@@ -8,6 +8,7 @@ import pytest
 from omnipy.data._display import (DefinedDimensions,
                                   Dimensions,
                                   DimensionsFit,
+                                  OutputConfig,
                                   pretty_repr,
                                   PrettyPrinterLib)
 from omnipy.data.model import Model
@@ -149,6 +150,73 @@ def test_dimensions_fit_immutable_properties(
 
     with pytest.raises(AttributeError):
         fit.both = False  # type: ignore
+
+
+def test_output_config(
+        skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
+    config = OutputConfig(
+        indent_tab_size=4, debug_mode=True, pretty_printer=PrettyPrinterLib.DEVTOOLS)
+    assert config.indent_tab_size == 4
+    assert config.debug_mode is True
+    assert config.pretty_printer is PrettyPrinterLib.DEVTOOLS
+
+    config = OutputConfig(
+        indent_tab_size=0,
+        debug_mode=False,
+        pretty_printer='rich',  # type: ignore[arg-type]
+    )
+    assert config.indent_tab_size == 0
+    assert config.debug_mode is False
+    assert config.pretty_printer is PrettyPrinterLib.RICH
+
+
+def test_output_config_mutable_properties(
+        skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
+    config = OutputConfig(indent_tab_size=4, debug_mode=True)
+
+    config.indent_tab_size = 3
+    assert config.indent_tab_size == 3
+
+    config.debug_mode = False
+    assert config.debug_mode is False
+
+    config.pretty_printer = PrettyPrinterLib.DEVTOOLS
+    assert config.pretty_printer is PrettyPrinterLib.DEVTOOLS
+
+
+def test_fail_output_config_if_invalid_params(
+        skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
+    with pytest.raises(ValueError):
+        OutputConfig(indent_tab_size=-1)
+
+    with pytest.raises(ValueError):
+        OutputConfig(indent_tab_size=None)  # type: ignore
+
+    with pytest.raises(ValueError):
+        OutputConfig(debug_mode=None)  # type: ignore
+
+    with pytest.raises(ValueError):
+        OutputConfig(pretty_printer=None)  # type: ignore
+
+
+def test_output_config_default_values(
+        skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
+    config = OutputConfig()
+    assert config.indent_tab_size == 2
+    assert config.debug_mode is False
+    assert config.pretty_printer is PrettyPrinterLib.RICH
+
+
+def test_fail_output_config_if_extra_params(
+        skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
+    with pytest.raises(TypeError):
+        OutputConfig(indent_tab_size=4, debug_mode=True, extra=2)  # type: ignore
+
+
+def test_fail_output_config_no_positional_parameters(
+        skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
+    with pytest.raises(TypeError):
+        OutputConfig(2, True)  # type: ignore
 
 
 def _harmonize(output: str) -> str:
