a18b8b6879e4b6da4a7f89d1fb5c4144315075ac
Sveinung Gundersen <sveinugu@gmail.com>
2025-02-12 17:14:37 +0100

    Flake8 ignore of entire _pydantic.py instead of each line

--- src/omnipy/util/_pydantic.py
+++ src/omnipy/util/_pydantic.py
@@ -1,27 +1,29 @@
-from pydantic import BaseConfig  # noqa
-from pydantic import BaseModel  # noqa
-from pydantic import ConfigDict  # noqa
-from pydantic import ConfigError  # noqa
-from pydantic import conint  # noqa
-from pydantic import constr  # noqa
-from pydantic import create_model  # noqa
-from pydantic import EmailStr  # noqa
-from pydantic import Extra  # noqa
-from pydantic import Field  # noqa
-from pydantic import NoneIsNotAllowedError  # noqa
-from pydantic import PositiveInt  # noqa
-from pydantic import PrivateAttr  # noqa
-from pydantic import Protocol  # noqa
-from pydantic import root_validator  # noqa
-from pydantic import StrictBytes  # noqa
-from pydantic import StrictInt  # noqa
-from pydantic import StrictStr  # noqa
-from pydantic import ValidationError  # noqa
-from pydantic import validator  # noqa
-from pydantic.dataclasses import dataclass  # noqa
-from pydantic.error_wrappers import ErrorWrapper  # noqa
-from pydantic.fields import ModelField, Undefined, UndefinedType  # noqa
-from pydantic.generics import GenericModel  # noqa
-from pydantic.main import ModelMetaclass, validate_model  # noqa
-from pydantic.typing import display_as_type, is_none_type  # noqa
-from pydantic.utils import lenient_isinstance, lenient_issubclass, sequence_like  # noqa
+# flake8: noqa
+
+from pydantic import (BaseConfig,
+                      BaseModel,
+                      ConfigDict,
+                      ConfigError,
+                      conint,
+                      constr,
+                      create_model,
+                      EmailStr,
+                      Extra,
+                      Field,
+                      NoneIsNotAllowedError,
+                      PositiveInt,
+                      PrivateAttr,
+                      Protocol,
+                      root_validator,
+                      StrictBytes,
+                      StrictInt,
+                      StrictStr,
+                      ValidationError,
+                      validator)
+from pydantic.dataclasses import dataclass
+from pydantic.error_wrappers import ErrorWrapper
+from pydantic.fields import ModelField, Undefined, UndefinedType
+from pydantic.generics import GenericModel
+from pydantic.main import ModelMetaclass, validate_model
+from pydantic.typing import display_as_type, is_none_type
+from pydantic.utils import lenient_isinstance, lenient_issubclass, sequence_like
