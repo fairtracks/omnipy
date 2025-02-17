58856a0b5fbfbd91d155076e8968ee241b3b384a
Sveinung Gundersen <sveinugu@gmail.com>
2025-02-17 09:47:02 +0100

    Added language and color_style options to OutputConfig

--- tests/data/display/test_config.py
+++ tests/data/display/test_config.py
@@ -2,25 +2,40 @@ from typing import Annotated
 
 import pytest
 
-from omnipy.data._display.config import OutputConfig, PrettyPrinterLib
+from omnipy.data._display.config import (HighContrastLightColorStyles,
+                                         LowerContrastDarkColorStyles,
+                                         OutputConfig,
+                                         PrettyPrinterLib,
+                                         SyntaxLanguage)
 
 
 def test_output_config(
         skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
     config = OutputConfig(
-        indent_tab_size=4, debug_mode=True, pretty_printer=PrettyPrinterLib.DEVTOOLS)
+        indent_tab_size=4,
+        debug_mode=True,
+        pretty_printer=PrettyPrinterLib.DEVTOOLS,
+        language=SyntaxLanguage.JSON,
+        color_style=LowerContrastDarkColorStyles.ONE_DARK,
+    )
     assert config.indent_tab_size == 4
     assert config.debug_mode is True
     assert config.pretty_printer is PrettyPrinterLib.DEVTOOLS
+    assert config.language is SyntaxLanguage.JSON
+    assert config.color_style is LowerContrastDarkColorStyles.ONE_DARK
 
     config = OutputConfig(
         indent_tab_size=0,
         debug_mode=False,
         pretty_printer='rich',  # type: ignore[arg-type]
+        language='xml',
+        color_style='xcode',
     )
     assert config.indent_tab_size == 0
     assert config.debug_mode is False
     assert config.pretty_printer is PrettyPrinterLib.RICH
+    assert config.language is SyntaxLanguage.XML
+    assert config.color_style is HighContrastLightColorStyles.XCODE
 
 
 def test_output_config_validate_assignments(
@@ -45,6 +60,29 @@ def test_output_config_validate_assignments(
     with pytest.raises(ValueError):
         config.pretty_printer = 'something'  # type: ignore[assignment]
 
+    config.language = None
+    assert config.language is None
+
+    # Any language string supported by the pygments library should be accepted
+    config.language = 'c++'
+    assert config.language == 'c++'
+
+    with pytest.raises(ValueError):
+        config.language = '123'
+
+    config.color_style = None
+    assert config.color_style is None
+
+    # Any color style string supported by the pygments library should be accepted
+    # Note: the lilypond color style is for use with the lilypond music notation software and as
+    #       thus excluded from the list of valid color styles in Omnipy, but it is still a valid
+    #       color style in the Pygments library.
+    config.color_style = 'lilypond'
+    assert config.color_style == 'lilypond'
+
+    with pytest.raises(ValueError):
+        config.color_style = '123'
+
 
 def test_fail_output_config_if_invalid_params(
         skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
@@ -60,6 +98,9 @@ def test_fail_output_config_if_invalid_params(
     with pytest.raises(ValueError):
         OutputConfig(pretty_printer=None)  # type: ignore
 
+    with pytest.raises(ValueError):
+        OutputConfig(language='xyz')
+
 
 def test_output_config_default_values(
         skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
@@ -67,6 +108,8 @@ def test_output_config_default_values(
     assert config.indent_tab_size == 2
     assert config.debug_mode is False
     assert config.pretty_printer is PrettyPrinterLib.RICH
+    assert config.language is SyntaxLanguage.PYTHON
+    assert config.color_style is HighContrastLightColorStyles.DEFAULT
 
 
 def test_fail_output_config_if_extra_params(
