dfb1910fb097639baf1de1706f405969c46b971e
Sveinung Gundersen <sveinugu@gmail.com>
2025-02-21 14:26:23 +0100

    Implemented virtual cropping for html output. Fixed tests

--- tests/data/display/test_styling.py
+++ tests/data/display/test_styling.py
@@ -1,5 +1,6 @@
+import re
 from textwrap import dedent
-from typing import Annotated
+from typing import Annotated, Callable
 
 import pytest
 import pytest_cases as pc
@@ -76,8 +77,40 @@ def test_stylized_output_immutable_properties(
         output.config = OutputConfig()
 
 
+def _strip_html(html: str) -> str:
+    matches = re.findall(r'<code[^>]*>([\S\s]*)</code>', html, re.MULTILINE)
+    if not matches:
+        return html
+
+    code_no_tags = re.sub(r'<[^>]+>', '', matches[0])
+
+    def _to_char(match: re.Match) -> str:
+        import sys
+        byte_as_str = match[1]
+        return int(byte_as_str, 16).to_bytes(length=1, byteorder=sys.byteorder).decode()
+
+    code_no_escapes = re.sub(r'&#x(\d+);', _to_char, code_no_tags)
+    return code_no_escapes
+
+
+def _strip_ansi(text: str) -> str:
+    return re.sub(r'\x1b\[[^m]+m', '', text)
+
+
+def _prepare_output(
+    output: StylizedMonospacedOutput,
+    get_output_property: Callable[[StylizedMonospacedOutput], str],
+) -> str:
+    return _strip_ansi(_strip_html(get_output_property(output)))
+
+
+@pc.parametrize_with_cases(
+    'output_prop_expectations', cases='.cases.styling', has_tag='expectations')
 def test_stylized_output_overflow_modes(
+        output_prop_expectations: Annotated[OutputPropertyExpectations, pc.fixture],
         skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]) -> None:
+
+    get_output_property, _ = output_prop_expectations
 
     content = ("[MyClass({'abc': [123, 234]}),\n"
                " MyClass({'def': [345, 456]})]")
@@ -87,7 +120,7 @@ def test_stylized_output_overflow_modes(
         frame=Frame(Dimensions(22, None)),
         config=OutputConfig(horizontal_overflow_mode=HorizontalOverflowMode.WORD_WRAP),
     )
-    assert output.plain.terminal == dedent("""\
+    assert _prepare_output(output, get_output_property) == dedent("""\
         [MyClass({'abc': [123,
         234]}),
          MyClass({'def': [345,
@@ -101,7 +134,7 @@ def test_stylized_output_overflow_modes(
         frame=Frame(Dimensions(22, None)),
         config=OutputConfig(horizontal_overflow_mode=HorizontalOverflowMode.ELLIPSIS),
     )
-    assert output.plain.terminal == dedent("""\
+    assert _prepare_output(output, get_output_property) == dedent("""\
         [MyClass({'abc': [123…
          MyClass({'def': [345…
         """)
@@ -113,7 +146,7 @@ def test_stylized_output_overflow_modes(
         frame=Frame(Dimensions(22, None)),
         config=OutputConfig(horizontal_overflow_mode=HorizontalOverflowMode.CROP),
     )
-    assert output.plain.terminal == dedent("""\
+    assert _prepare_output(output, get_output_property) == dedent("""\
         [MyClass({'abc': [123,
          MyClass({'def': [345,
         """)
@@ -125,7 +158,7 @@ def test_stylized_output_overflow_modes(
         frame=Frame(Dimensions(10, 8)),
         config=OutputConfig(horizontal_overflow_mode=HorizontalOverflowMode.WORD_WRAP),
     )
-    assert output.plain.terminal == dedent("""\
+    assert _prepare_output(output, get_output_property) == dedent("""\
         [MyClass({
         'abc': 
         [123, 
@@ -145,7 +178,7 @@ def test_stylized_output_overflow_modes(
             horizontal_overflow_mode=HorizontalOverflowMode.WORD_WRAP,
             vertical_overflow_mode=VerticalOverflowMode.CROP_BOTTOM,
         ))
-    assert output.plain.terminal == dedent("""\
+    assert _prepare_output(output, get_output_property) == dedent("""\
         [MyClass({
         'abc': 
         [123, 
@@ -161,7 +194,7 @@ def test_stylized_output_overflow_modes(
             horizontal_overflow_mode=HorizontalOverflowMode.WORD_WRAP,
             vertical_overflow_mode=VerticalOverflowMode.CROP_TOP,
         ))
-    assert output.plain.terminal == dedent("""\
+    assert _prepare_output(output, get_output_property) == dedent("""\
         456]})]
         """)  # noqa: W291
     assert output.within_frame.width is True
