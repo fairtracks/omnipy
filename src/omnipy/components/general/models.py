0a7b32f984df4547d7f8f734bcb46406b417c6ba
Sveinung Gundersen <sveinugu@gmail.com>
2026-01-03 13:18:26 +0100

    Fix typing of ChainX models

--- src/omnipy/components/general/models.py
+++ src/omnipy/components/general/models.py
@@ -1,10 +1,11 @@
 import typing
-from typing import Generic, get_args
+from typing import Any, Generic, get_args, Protocol
 
 from typing_extensions import TypeVar
 
 from omnipy.data.helpers import TypeVarStore1, TypeVarStore2, TypeVarStore3, TypeVarStore4
 from omnipy.data.model import Model
+from omnipy.shared.typedefs import TypeForm
 from omnipy.util.helpers import is_iterable
 
 
@@ -54,9 +55,15 @@ _Y = TypeVar('_Y', bound=Model, default=Model[object])
 _Z = TypeVar('_Z', bound=Model, default=Model[object])
 
 
+class HasOuterType(Protocol):
+    @classmethod
+    def outer_type(cls, with_args: bool = False) -> TypeForm:
+        ...
+
+
 class _ChainMixin:
     @classmethod
-    def _parse_data(cls, data: object) -> object:
+    def _parse_data(cls: type[HasOuterType], data: Any) -> Any:
         type_args = get_args(cls.outer_type(with_args=True))
         store_models = [get_args(store)[0] for store in type_args[1:-1]]
         all_models = [type_args[0]] + store_models + [type_args[-1]]
@@ -73,33 +80,35 @@ class _ChainMixin:
         return data
 
 
-class Chain2(_ChainMixin, Model[_V | _U], Generic[_U, _V]):
-    ...
-
-
 if typing.TYPE_CHECKING:
 
-    class Chain3(Model[_W], Generic[_U, _V, _W]):
+    class Chain2(_ChainMixin, Model[_V], Generic[_U, _V]):
         ...
 
-    class Chain4(_ChainMixin, Model[_X | _W | _V | _U], Generic[_U, _V, _W, _X]):
+    class Chain3(_ChainMixin, Model[_W], Generic[_U, _V, _W]):
+        ...
+
+    class Chain4(_ChainMixin, Model[_X], Generic[_U, _V, _W, _X]):
         ...
 
     class Chain5(
             _ChainMixin,
-            Model[_Y | _X | _W | _V | _U],
+            Model[_Y],
             Generic[_U, _V, _W, _X, _Y],
     ):
         ...
 
     class Chain6(
             _ChainMixin,
-            Model[_Z | _Y | _X | _W | _V | _U],
+            Model[_Z],
             Generic[_U, _V, _W, _X, _Y, _Z],
     ):
         ...
 
 else:
+
+    class Chain2(_ChainMixin, Model[_V | _U], Generic[_U, _V]):
+        ...
 
     class Chain3(_ChainMixin, Model[_W | TypeVarStore1[_V] | _U], Generic[_U, _V, _W]):
         ...
