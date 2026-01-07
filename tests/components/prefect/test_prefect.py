689fc0b5eecafde3134b85da96ad68391426400c
Sveinung Gundersen <sveinugu@gmail.com>
2026-01-07 19:39:39 +0100

    Removed unneeded tests

--- tests/components/prefect/test_prefect.py
+++ tests/components/prefect/test_prefect.py
@@ -1,9 +1,13 @@
 import logging
 import os
+from typing import Annotated
+
+import pytest
 
 
-def test_no_prefect_console_handler_in_root_logger():
-    import omnipy.components.prefect  # noqa: F401
+def test_no_prefect_console_handler_in_root_logger(
+        skip_test_if_not_default_data_config_values: Annotated[None, pytest.fixture]):
+    import omnipy.components.prefect.lazy_import  # noqa: F401
 
     assert os.environ['PREFECT_LOGGING_SETTINGS_PATH'].endswith('logging.yml')
 
@@ -13,8 +17,9 @@ def test_no_prefect_console_handler_in_root_logger():
     assert not any(isinstance(handler, PrefectConsoleHandler) for handler in logging.root.handlers)
 
 
-def test_only_local_orion():
-    import omnipy.components.prefect  # noqa: F401
+def test_only_local_orion(skip_test_if_not_default_data_config_values: Annotated[None,
+                                                                                 pytest.fixture]):
+    import omnipy.components.prefect.lazy_import  # noqa: F401
 
     assert os.environ['PREFECT_API_KEY'] == ''
     assert os.environ['PREFECT_API_URL'] == ''
