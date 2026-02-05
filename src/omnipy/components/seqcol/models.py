c306f05b46658b3568b5338afe1a7b3f00f5110d
Sveinung Gundersen <sveinugu@gmail.com>
2026-02-05 16:05:23 +0100

    Added validation of all level 2 fields, one test failing

--- src/omnipy/components/seqcol/models.py
+++ src/omnipy/components/seqcol/models.py
@@ -1,5 +1,8 @@
+from typing import Any
+
 from omnipy.data.model import Model
 import omnipy.util._pydantic as pyd
+# from omnipy.util.helpers import first_value_in_mapping
 
 
 class NameLengthPair(pyd.BaseModel):
@@ -11,8 +14,53 @@ class SeqColLevel2Record(pyd.BaseModel):
     lengths: list[int]
     names: list[str]
     sequences: list[str]
-    sorted_sequences: list[str]
-    name_length_pairs: list[NameLengthPair]
+    sorted_sequences: list[str] = pyd.Field(default_factory=list)
+    name_length_pairs: list[NameLengthPair] = pyd.Field(default_factory=list)
+
+    @pyd.validator('sorted_sequences')
+    def set_sorted_sequences(cls, v: list[str], values) -> list[str]:
+        if v:
+            prev_val = v[0]
+            for seq in v[1:]:
+                if seq < prev_val:
+                    raise ValueError('sorted_sequences must be sorted')
+                prev_val = seq
+            return v
+        else:
+            return sorted(values['sequences'])
+
+    @pyd.validator('name_length_pairs')
+    def check_name_length_pairs(cls, v: list[NameLengthPair],
+                                values: dict[str, Any]) -> list[NameLengthPair]:
+        if v:
+            for i, name_length_pair in enumerate(v):
+                assert name_length_pair.name == values['names'][i], \
+                    (f'name_length_pairs[{i}].name must match names[{i}], '
+                     f'{name_length_pair.name} != {values["names"][i]}')
+
+                assert name_length_pair.length == values['lengths'][i], \
+                    (f'name_length_pairs[{i}].length must match lengths[{i}], '
+                     f'{name_length_pair.length} != {values["lengths"][i]}')
+            return v
+        else:
+            return [
+                NameLengthPair(name=name, length=length)
+                for name, length in zip(values['names'], values['lengths'])
+            ]
+
+    # @pyd.validator(
+    #     'names',
+    #     'sequences',
+    #     'sorted_sequences',
+    #     'name_length_pairs',
+    # )
+    # def check_equal_length_lists(cls, v: list[Any], values: dict[str, Any]) -> dict[str, Any]:
+    #     # Currently assumes that all fields are lists
+    #     first_length = len(first_value_in_mapping(values))
+    #     # assert all(len(val) == first_length for val in values.values()), \
+    #     assert len(v) == first_length, \
+    #         f'Length of fields must be equal: {({key:len(val) for key, val in values.items()})}'
+    #     return values
 
 
 class SeqColLevel2Model(Model[SeqColLevel2Record]):
--- tests/components/seqcol/test_seqcol.py
+++ tests/components/seqcol/test_seqcol.py
@@ -1,4 +1,5 @@
-from typing import Annotated
+from copy import copy
+from typing import Annotated, Any
 
 import pytest
 
@@ -23,7 +24,7 @@ def seqcol_level_1_data() -> dict:
 
 
 @pytest.fixture
-def seqcol_level_2_data() -> dict:
+def seqcol_level_2_required_data() -> dict:
     return {
         'lengths': [8, 4, 4],
         'names': ['X', '1', '2'],
@@ -32,24 +33,29 @@ def seqcol_level_2_data() -> dict:
             'SQ.YBbVX0dLKG1ieEDCiMmkrTZFt_Z5Vdaj',
             'SQ.AcLxtBuKEPk_7PGE_H4dGElwZHCujwH6'
         ],
-        'sorted_sequences': [
-            'SQ.AcLxtBuKEPk_7PGE_H4dGElwZHCujwH6',
-            'SQ.YBbVX0dLKG1ieEDCiMmkrTZFt_Z5Vdaj',
-            'SQ.iYtREV555dUFKg2_agSJW6suquUyPpMw'
-        ],
-        'name_length_pairs': [{
-            'length': 8, 'name': 'X'
-        }, {
-            'length': 4, 'name': '1'
-        }, {
-            'length': 4, 'name': '2'
-        }]
     }
 
 
-def test_seqcol_level_2_model(seqcol_level_2_data: Annotated[dict, pytest.fixture]) -> None:
-    seqcol_level_2_model = SeqColLevel2Model(seqcol_level_2_data)
-    assert seqcol_level_2_model.to_data() == seqcol_level_2_data
+@pytest.fixture
+def seqcol_level_2_full_data(seqcol_level_2_required_data: Annotated[dict, pytest.fixture]) -> dict:
+    seqcol_level_2_required_data['sorted_sequences'] = [
+        'SQ.AcLxtBuKEPk_7PGE_H4dGElwZHCujwH6',
+        'SQ.YBbVX0dLKG1ieEDCiMmkrTZFt_Z5Vdaj',
+        'SQ.iYtREV555dUFKg2_agSJW6suquUyPpMw'
+    ]
+    seqcol_level_2_required_data['name_length_pairs'] = [{
+        'length': 8, 'name': 'X'
+    }, {
+        'length': 4, 'name': '1'
+    }, {
+        'length': 4, 'name': '2'
+    }]
+    return seqcol_level_2_required_data
+
+
+def test_seqcol_level_2_model(seqcol_level_2_full_data: Annotated[dict, pytest.fixture]) -> None:
+    seqcol_level_2_model = SeqColLevel2Model(seqcol_level_2_full_data)
+    assert seqcol_level_2_model.to_data() == seqcol_level_2_full_data
     assert seqcol_level_2_model.lengths == [8, 4, 4]  # type: ignore[attr-defined]
     assert seqcol_level_2_model.names == ['X', '1', '2']  # type: ignore[attr-defined]
     assert (seqcol_level_2_model.sequences[0]  # type: ignore[attr-defined]
@@ -58,3 +64,94 @@ def test_seqcol_level_2_model(seqcol_level_2_data: Annotated[dict, pytest.fixtur
             == 'SQ.AcLxtBuKEPk_7PGE_H4dGElwZHCujwH6')
     assert seqcol_level_2_model.name_length_pairs[0].length == 8  # type: ignore[attr-defined]
     assert seqcol_level_2_model.name_length_pairs[0].name == 'X'  # type: ignore[attr-defined]
+
+
+@pytest.mark.parametrize(
+    'append_data',
+    [
+        ('lengths', 10),
+        ('names', '3'),
+        ('sequences', 'SQ.1234567890'),
+        ('sorted_sequences', 'SQ.1234567890'),
+        ('name_length_pairs', {
+            'length': 10, 'name': '3'
+        }),
+    ],
+    ids=['lengths', 'names', 'sequences', 'sorted_sequences', 'name_length_pairs'])
+def test_seqcol_level_2_model_validate_equal_length_lists(
+    seqcol_level_2_full_data: Annotated[dict, pytest.fixture],
+    append_data: tuple[str, Any],
+) -> None:
+    key, append_val = append_data
+    seqcol_level_2_non_equal_length_data = copy(seqcol_level_2_full_data)
+    seqcol_level_2_non_equal_length_data[key].append(append_val)  # type: ignore[attr-defined]
+    with pytest.raises(ValueError):
+        SeqColLevel2Model(seqcol_level_2_non_equal_length_data)
+
+
+def test_seqcol_level_2_model_validate_sorted_sequences(
+        seqcol_level_2_required_data: Annotated[dict, pytest.fixture]) -> None:
+    seqcol_level_2_incorrectly_sorted_data = seqcol_level_2_required_data
+    seqcol_level_2_incorrectly_sorted_data['sorted_sequences'] = [
+        'SQ.iYtREV555dUFKg2_agSJW6suquUyPpMw',
+        'SQ.YBbVX0dLKG1ieEDCiMmkrTZFt_Z5Vdaj'
+        'SQ.AcLxtBuKEPk_7PGE_H4dGElwZHCujwH6',
+    ]
+    with pytest.raises(ValueError):
+        SeqColLevel2Model(seqcol_level_2_incorrectly_sorted_data)
+
+
+def test_seqcol_level_2_model_missing_sorted_sequences(
+        seqcol_level_2_required_data: Annotated[dict, pytest.fixture]) -> None:
+    seqcol_level_2_model = SeqColLevel2Model(seqcol_level_2_required_data)
+    assert (seqcol_level_2_model.sorted_sequences  # type: ignore[attr-defined]
+            == [
+                'SQ.AcLxtBuKEPk_7PGE_H4dGElwZHCujwH6',
+                'SQ.YBbVX0dLKG1ieEDCiMmkrTZFt_Z5Vdaj',
+                'SQ.iYtREV555dUFKg2_agSJW6suquUyPpMw'
+            ])
+
+
+def test_seqcol_level_2_model_validate_name_length_pairs(
+        seqcol_level_2_full_data: Annotated[dict, pytest.fixture]) -> None:
+    seqcol_level_2_incorrect_name_data = copy(seqcol_level_2_full_data)
+    seqcol_level_2_incorrect_name_data['name_length_pairs'][0]['name'] = 'Y'
+    with pytest.raises(ValueError):
+        SeqColLevel2Model(seqcol_level_2_incorrect_name_data)
+
+    seqcol_level_2_incorrect_length_data = copy(seqcol_level_2_full_data)
+    seqcol_level_2_incorrect_length_data['name_length_pairs'][1]['length'] = 10
+    with pytest.raises(ValueError):
+        SeqColLevel2Model(seqcol_level_2_incorrect_length_data)
+
+    seqcol_level_2_incorrect_list_length_data = copy(seqcol_level_2_full_data)
+    seqcol_level_2_incorrect_length_data['name_length_pairs'] = \
+        seqcol_level_2_incorrect_length_data['name_length_pairs'][:-1]
+    with pytest.raises(ValueError):
+        SeqColLevel2Model(seqcol_level_2_incorrect_list_length_data)
+
+
+def test_seqcol_level_2_model_missing_name_length_pairs(
+        seqcol_level_2_required_data: Annotated[dict, pytest.fixture]) -> None:
+    seqcol_level_2_model = SeqColLevel2Model(seqcol_level_2_required_data)
+    assert (seqcol_level_2_model.name_length_pairs  # type: ignore[attr-defined]
+            == [{
+                'length': 8, 'name': 'X'
+            }, {
+                'length': 4, 'name': '1'
+            }, {
+                'length': 4, 'name': '2'
+            }])
+
+
+def test_seqcol_level_2_model_invalid_data() -> None:
+    with pytest.raises(ValueError):
+        SeqColLevel2Model({
+            'lengths': ['something', 4, 4],
+            'names': ['X', '1', '2'],
+            'sequences': [
+                'SQ.iYtREV555dUFKg2_agSJW6suquUyPpMw',
+                'SQ.YBbVX0dLKG1ieEDCiMmkrTZFt_Z5Vdaj',
+                'SQ.AcLxtBuKEPk_7PGE_H4dGElwZHCujwH6'
+            ]
+        })
