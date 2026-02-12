2f2d446e6d60ace13f37112b4a2b8da3ca1e7c1a
Sveinung Gundersen <sveinugu@gmail.com>
2026-02-12 13:19:35 +0100

    Implemented length equality validation for names, lengths, sequences, and name_length_pairs. Improved type checking of validators

--- src/omnipy/components/seqcol/models.py
+++ src/omnipy/components/seqcol/models.py
@@ -1,9 +1,35 @@
-from typing import Any
+from typing_extensions import TypedDict
 
 from omnipy.data.model import Model
 import omnipy.util._pydantic as pyd
+from omnipy.util.helpers import first_key_in_mapping, first_value_in_mapping
 
-# from omnipy.util.helpers import first_value_in_mapping
+
+# Currently needed for type checking of the validators, but unfortunately
+# violates the DRY principle (Don't Repeat Yourself).
+class NameLengthPairDict(TypedDict):
+    name: str
+    length: int
+
+
+class SeqColLevel2Dict(TypedDict, extra_items=list[object]):
+    lengths: list[int]
+    names: list[str]
+    sequences: list[str]
+    sorted_sequences: list[str]
+    name_length_pairs: list[NameLengthPairDict]
+
+
+def check_equally_long_as_first(value: list[object],
+                                values: SeqColLevel2Dict,
+                                field: pyd.ModelField) -> list[object]:
+    # Currently assumes that all fields are lists
+    first_length = len(first_value_in_mapping(values))
+    assert len(value) == first_length, \
+        (f'Length of fields must be equal, but the length of fields '
+         f'{first_key_in_mapping(values)} and {field.name} are not equal: '
+         f'{first_length} != {len(value)}')
+    return value
 
 
 class NameLengthPair(pyd.BaseModel):
@@ -19,7 +45,7 @@ class SeqColLevel2Record(pyd.BaseModel):
     name_length_pairs: list[NameLengthPair] = pyd.Field(default_factory=list)
 
     @pyd.validator('sorted_sequences')
-    def set_sorted_sequences(cls, v: list[str], values) -> list[str]:
+    def set_sorted_sequences(cls, v: list[str], values: SeqColLevel2Dict) -> list[str]:
         if v:
             prev_val = v[0]
             for seq in v[1:]:
@@ -32,16 +58,21 @@ class SeqColLevel2Record(pyd.BaseModel):
 
     @pyd.validator('name_length_pairs')
     def check_name_length_pairs(cls, v: list[NameLengthPair],
-                                values: dict[str, Any]) -> list[NameLengthPair]:
+                                values: SeqColLevel2Dict) -> list[NameLengthPair]:
+        # To avoid key errors if e.g. check_equally_long_as_first fails
+        assert all(_ in values for _ in ['names', 'lengths'])
+
         if v:
             for i, name_length_pair in enumerate(v):
-                assert name_length_pair.name == values['names'][i], \
-                    (f'name_length_pairs[{i}].name must match names[{i}], '
-                     f'{name_length_pair.name} != {values["names"][i]}')
+                if i < len(values['names']):
+                    assert name_length_pair.name == values['names'][i], \
+                        (f'name_length_pairs[{i}].name must match names[{i}], '
+                         f'{name_length_pair.name} != {values["names"][i]}')
 
-                assert name_length_pair.length == values['lengths'][i], \
-                    (f'name_length_pairs[{i}].length must match lengths[{i}], '
-                     f'{name_length_pair.length} != {values["lengths"][i]}')
+                if i < len(values['lengths']):
+                    assert name_length_pair.length == values['lengths'][i], \
+                        (f'name_length_pairs[{i}].length must match lengths[{i}], '
+                         f'{name_length_pair.length} != {values["lengths"][i]}')
             return v
         else:
             return [
@@ -49,19 +80,14 @@ class SeqColLevel2Record(pyd.BaseModel):
                 for name, length in zip(values['names'], values['lengths'])
             ]
 
-    # @pyd.validator(
-    #     'names',
-    #     'sequences',
-    #     'sorted_sequences',
-    #     'name_length_pairs',
-    # )
-    # def check_equal_length_lists(cls, v: list[Any], values: dict[str, Any]) -> dict[str, Any]:
-    #     # Currently assumes that all fields are lists
-    #     first_length = len(first_value_in_mapping(values))
-    #     # assert all(len(val) == first_length for val in values.values()), \
-    #     assert len(v) == first_length, \
-    #         f'Length of fields must be equal: {({key:len(val) for key, val in values.items()})}'
-    #     return values
+    _names_list_length_equality_validator = pyd.validator(
+        'names', allow_reuse=True)(check_equally_long_as_first,)
+    _sequences_list_length_equality_validator = pyd.validator(
+        'sequences', allow_reuse=True)(check_equally_long_as_first,)
+    _sorted_sequences_list_length_equality_validator = pyd.validator(
+        'sorted_sequences', allow_reuse=True)(check_equally_long_as_first,)
+    _name_length_pairs_list_length_equality_validator = pyd.validator(
+        'name_length_pairs', allow_reuse=True)(check_equally_long_as_first,)
 
 
 class SeqColLevel2Model(Model[SeqColLevel2Record]):
