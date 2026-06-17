262feaa3fab17a821743384d3a39cd88275ff13d
Sveinung Gundersen <sveinugu@gmail.com>
2026-06-17 21:20:27 +0200

    Update chardet to >7, adjust test cases

--- tests/components/raw/test_tasks.py
+++ tests/components/raw/test_tasks.py
@@ -19,11 +19,9 @@ def test_decode_bytes(runtime: Annotated[IsRuntime, pytest.fixture]) -> None:
         DecodeCaseInfo(b'ASCII string', 'ASCII string', 'ascii'),
         DecodeCaseInfo(b'\xc3\xa6\xc3\xb8\xc3\xa5\xc3\x97\xe2\x80\xa0', 'æøå×†', 'utf-8'),
         DecodeCaseInfo(b'\xff\xfe\xe6\x00\xf8\x00\xe5\x00\xd7\x00  ', 'æøå×†', 'utf-16'),
-        DecodeCaseInfo(b'\xd7 \xc6re v\xe6re \xf8let v\xe5rt! \xd7',
-                       '× Ære være ølet vårt! ×',
-                       'latin-1'),
-        DecodeCaseInfo(b'\xd7 \xc6re v\xe6re \xf8let\x86 v\xe5rt! \xd7',
-                       '× Ære være ølet† vårt! ×',
+        DecodeCaseInfo(b'\xc6re v\xe6re \xf8let v\xe5rt!', 'Ære være ølet vårt!', 'latin-1'),
+        DecodeCaseInfo(b'\xc6re v\xe6re \xf8let\x86 v\xe5rt!',
+                       'Ære være ølet† vårt!',
                        'windows-1252'),
     ]
     for case in test_cases:
