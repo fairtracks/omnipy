9398afbdde748f7d36df62f15325788dd0d8e7c6
Sveinung Gundersen <sveinugu@gmail.com>
2026-07-07 10:55:44 +0200

    Removed xfail on two previously failing Prefect engine tests.

--- tests/engine/cases/tasks.py
+++ tests/engine/cases/tasks.py
@@ -261,12 +261,6 @@ def case_async_wait_a_bit_multithreaded_threading() -> JobCase[[float], Awaitabl
 @pytest.mark.asyncio
 def case_async_wait_a_bit_multithreaded_futures() -> JobCase[[float], Awaitable[float]]:
     async def run_and_assert_results(job: IsJob) -> None:
-        from omnipy.components.prefect.engine.prefect import PrefectEngine
-        if any(_ in job.__class__.__name__ for _ in ['LinearFlow', 'DagFlow']) \
-                and check_engine_cls(job, PrefectEngine):
-            pytest.xfail('Stopped working in some Prefect version between 2.10.10 and 2.13.3.'
-                         '(Unclear if the above comment is specific to this test case, as all'
-                         'tests in test_all_engines.py were disabled).')
         future = await asyncio.get_running_loop().run_in_executor(None, job, 0.005)
         sync_wait_for_job_state(job, [RunState.RUNNING, RunState.FINISHED])
 
