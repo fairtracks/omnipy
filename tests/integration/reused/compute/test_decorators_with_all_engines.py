c27893948c76678a89aedc6a571ff21235ca1142
Sveinung Gundersen <sveinugu@gmail.com>
2026-07-11 12:44:29 +0200

    Add type ignore comments for undefined attributes in test decorators.

--- tests/integration/reused/compute/test_decorators_with_all_engines.py
+++ tests/integration/reused/compute/test_decorators_with_all_engines.py
@@ -1,10 +1,11 @@
-from ....compute.test_decorators import dag_flow_plus_five_number_  # noqa
-from ....compute.test_decorators import dag_flow_plus_five_x_  # noqa
-from ....compute.test_decorators import func_flow_plus_func_x_y_  # noqa
-from ....compute.test_decorators import func_flow_plus_y_number_y_  # noqa
+from ....compute.test_decorators import dag_flow_plus_five_number_  # type: ignore[attr-defined]
+from ....compute.test_decorators import dag_flow_plus_five_x_  # type: ignore[attr-defined]
+from ....compute.test_decorators import func_flow_plus_func_x_y_  # type: ignore[attr-defined]
+from ....compute.test_decorators import func_flow_plus_y_number_y_  # type: ignore[attr-defined]
 from ....compute.test_decorators import \
-    test_dag_flow_template_as_decorator_plus_five_template  # noqa
-from ....compute.test_decorators import test_func_flow_template_as_decorator_plus_y_template  # noqa
+    test_dag_flow_template_as_decorator_plus_five_template  # type: ignore[attr-defined]
+from ....compute.test_decorators import \
+    test_func_flow_template_as_decorator_plus_y_template  # type: ignore[attr-defined]
 from ....compute.test_decorators import (test_dag_flow_template_as_decorator,
                                          test_fail_func_flow_template_decorator_with_func_argument,
                                          test_func_flow_template_as_decorator,
@@ -12,6 +13,7 @@ from ....compute.test_decorators import (test_dag_flow_template_as_decorator,
 
 dag_flow_plus_five_number_  # noqa
 dag_flow_plus_five_x_  # noqa
+func_flow_plus_func_x_y_  # noqa
 func_flow_plus_y_number_y_  # noqa
 func_flow_plus_y_number_y_  # noqa
 test_dag_flow_template_as_decorator_plus_five_template  # noqa
