f282c6a55bfe70c802e099f4ed46cb6523b26f03
Sveinung Gundersen <sveinugu@gmail.com>
2026-07-11 16:01:27 +0200

    Readd flake8 ignore

--- tests/integration/reused/engine/test_all_engines_with_real_jobs_and_registry.py
+++ tests/integration/reused/engine/test_all_engines_with_real_jobs_and_registry.py
@@ -1,5 +1,5 @@
 from ....engine.test_all_engines import \
-    test_mock_tasks_all_engines_mock_registry_job_case  # type: ignore[attr-defined]
+    test_mock_tasks_all_engines_mock_registry_job_case  # type: ignore[attr-defined]  # noqa
 from ....engine.test_all_engines import test_mock_tasks_all_engines_mock_registry
 
 test_mock_tasks_all_engines_mock_registry  # noqa
