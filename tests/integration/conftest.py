0c216ed34a9591e739d149d2cf1d527da6bca38c
Sveinung Gundersen <sveinugu@gmail.com>
2026-08-24 23:05:01 +0200

    Depend on only TMPDIR and PREFECT_TEST_PORT, both set outside of test code.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,6 @@
 """Shared fixtures for integration tests."""
 
 import os
-from pathlib import Path
 from typing import Annotated
 from unittest.mock import patch
 
@@ -13,9 +12,6 @@ from omnipy.components.prefect.lazy_import import prefect_test_harness
 from omnipy.shared.enums.job import EngineChoice
 from omnipy.shared.protocols.hub.runtime import IsRuntime
 
-_PREFECT_SQLITE_TMPDIR = '/tmp/omnipy-prefect-sqlite-tmp'
-_PREFECT_TEST_PORT = '40000'
-
 
 @pc.fixture(scope='function')
 @pc.parametrize(engine=[EngineChoice.LOCAL, EngineChoice.PREFECT], ids=['local', 'prefect'])
@@ -26,13 +22,9 @@ def runtime_all_engines(runtime: Annotated[IsRuntime, pytest.fixture], engine: s
 
 @pytest.fixture(autouse=True, scope='package')
 def prefect_test_fixture():
-    Path(_PREFECT_SQLITE_TMPDIR).mkdir(parents=True, exist_ok=True)
-
     with temporary_settings({PREFECT_FLOWS_HEARTBEAT_FREQUENCY: None}):
         prefect_test_envs = {
-            'TMPDIR': _PREFECT_SQLITE_TMPDIR,
-            'SQLITE_TMPDIR': _PREFECT_SQLITE_TMPDIR,
-            'PREFECT_TEST_PORT': _PREFECT_TEST_PORT,
+            'SQLITE_TMPDIR': os.getenv('TMPDIR', '/tmp'),
         }
         with patch.dict(os.environ, prefect_test_envs):
             with prefect_test_harness():
